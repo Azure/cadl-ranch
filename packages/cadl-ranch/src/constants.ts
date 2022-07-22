546381db9e910075b3b51ba02f79523348b86e70
Timothee Guerin <tiguerin@microsoft.com>
2022-07-22 14:18:46 -0700

    Basic implementation of mock apis

--- packages/cadl-ranch/src/constants.ts
+++ packages/cadl-ranch/src/constants.ts
@@ -1,7 +1,3 @@
-import { join, resolve } from "path";
-
-export const ProjectRoot = resolve(join(__dirname), "..");
-
 export const AdminUrls = {
   stop: "/.admin/stop",
 };
--- packages/cadl-ranch/src/server/index.ts
+++ packages/cadl-ranch/src/server/index.ts
@@ -1,2 +1,1 @@
 export * from "./server.js";
-export * from "./request-ext.js";
