1dbe0df3c5e2c6a20a3fb60fe1e197d98efeae37
msyyc <70930885+msyyc@users.noreply.github.com>
2022-08-09 16:45:34 +0800

    update

--- packages/cadl-ranch-specs/http/from-testserver/non-string-enum/mockapi.ts
+++ packages/cadl-ranch-specs/http/from-testserver/non-string-enum/mockapi.ts
@@ -1,4 +1,4 @@
-import { passOnSuccess, mockapi } from "@azure-tools/cadl-ranch-api";
+import { passOnSuccess, mockapi, json } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
@@ -6,67 +6,31 @@ export const Scenarios: Record<string, ScenarioMockApi> = {};
 Scenarios.Int_put = passOnSuccess(
   mockapi.put("/nonStringEnums/int/put", (req) => {
     if (req.body == "200") {
-      return {
-        status: 200,
-        body: {
-          contentType: "application/json",
-          rawContent: `"Nice job posting an int enum"`,
-        },
-      };
+      return { status: 200, body: json("Nice job posting an int enum") };
     } else {
-      return {
-        status: 400,
-        body: {
-          contentType: "application/json",
-          rawContent: `"Did not receive what I was expecting"`,
-        },
-      };
+      return { status: 400, body: json("Did not receive what I was expecting") };
     }
   }),
 );
 
 Scenarios.Int_get = passOnSuccess(
   mockapi.get("/nonStringEnums/int/get", () => {
-    return {
-      status: 200,
-      body: {
-        contentType: "application/json",
-        rawContent: `429`,
-      },
-    };
+    return { status: 200, body: json(429) };
   }),
 );
 
 Scenarios.Float_put = passOnSuccess(
   mockapi.put("/nonStringEnums/float/put", (req) => {
     if (req.body == "200.4") {
-      return {
-        status: 200,
-        body: {
-          contentType: "application/json",
-          rawContent: `"Nice job posting a float enum"`,
-        },
-      };
+      return { status: 200, body: json("Nice job posting a float enum") };
     } else {
-      return {
-        status: 400,
-        body: {
-          contentType: "application/json",
-          rawContent: `"Did not receive what I was expecting"`,
-        },
-      };
+      return { status: 400, body: json("Did not receive what I was expecting") };
     }
   }),
 );
 
 Scenarios.Float_get = passOnSuccess(
   mockapi.get("/nonStringEnums/float/get", () => {
-    return {
-      status: 200,
-      body: {
-        contentType: "application/json",
-        rawContent: `429.1`,
-      },
-    };
+    return { status: 200, body: json(429.1) };
   }),
 );
