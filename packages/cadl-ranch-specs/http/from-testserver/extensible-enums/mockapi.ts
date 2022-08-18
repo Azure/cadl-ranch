3bcda803f95affacf2c8e1bb759103cbbd94e59b
Timothee Guerin <tiguerin@microsoft.com>
2022-08-18 09:49:12 -0700

    Fix extensible enusm

--- packages/cadl-ranch-specs/http/from-testserver/extensible-enums/mockapi.ts
+++ packages/cadl-ranch-specs/http/from-testserver/extensible-enums/mockapi.ts
@@ -3,47 +3,30 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-// valid enums
-const tommy = {
-  DaysOfWeek: "Monday",
-  IntEnum: "1",
-  name: "Tommy Tomson",
-};
-// unexpected enum
-const casper = {
-  DaysOfWeek: "Weekend",
-  IntEnum: "2",
-  name: "Casper Ghosty",
-};
-// enum from allowed values
-const scooby = {
-  DaysOfWeek: "Thursday",
-  IntEnum: "2.1",
-  name: "Scooby Scarface",
-};
-Scenarios.ExtensibleEnums_getByPetId = passOnSuccess(
-  mockapi.get("/extensibleenums/pet/:petId", (req) => {
-    const petId = req.params.petId;
-    switch (petId) {
-      case "tommy":
-        return { status: 200, body: json(tommy) };
-      case "casper":
-        return { status: 200, body: json(casper) };
-      case "scooby":
-        return { status: 200, body: json(scooby) };
-      default:
-        return { status: 400, body: json("Pet not found for " + petId) };
-    }
+// Known Values
+Scenarios.ExtensibleEnums_String_getKnownValue = passOnSuccess(
+  mockapi.get("/extensible-enums/string/known-value", (req) => {
+    return { status: 200, body: json("Monday") };
   }),
 );
 
-Scenarios.ExtensibleEnums_addPet = passOnSuccess(
-  mockapi.post("/extensibleenums/pet/addPet", (req) => {
-    const petName = req.body.name;
-    if (petName === "Retriever") {
-      return { status: 200, body: json(req.body) };
-    } else {
-      return { status: 400, body: json("Pet info incorrect " + petName) };
-    }
+Scenarios.ExtensibleEnums_String_putKnownValue = passOnSuccess(
+  mockapi.get("/extensible-enums/string/known-value", (req) => {
+    req.expect.bodyEquals("Monday");
+    return { status: 204 };
+  }),
+);
+
+// Unknown values
+Scenarios.ExtensibleEnums_String_getUnknownValue = passOnSuccess(
+  mockapi.get("/extensible-enums/string/known-value", (req) => {
+    return { status: 200, body: json("Weekend") };
+  }),
+);
+
+Scenarios.ExtensibleEnums_String_putUnknownValue = passOnSuccess(
+  mockapi.get("/extensible-enums/string/known-value", (req) => {
+    req.expect.bodyEquals("Weekend");
+    return { status: 204 };
   }),
 );
