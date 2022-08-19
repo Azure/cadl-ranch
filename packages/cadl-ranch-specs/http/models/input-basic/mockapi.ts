aa782b96539185cae59ae85aafe283e04a225056
Haoling Dong <haolingdong@microsoft.com>
2022-08-19 14:52:54 +0800

    Fix Pretifier error

--- packages/cadl-ranch-specs/http/models/input-basic/mockapi.ts
+++ packages/cadl-ranch-specs/http/models/input-basic/mockapi.ts
@@ -6,7 +6,7 @@ Scenarios.InputBasic_getModel = passOnSuccess(
   mockapi.get("/input-basic/models", (req) => {
     if (req.body && req.body["requiredString"] && req.body["requiredInt"]) {
       return {
-        status: 200
+        status: 200,
       };
     } else {
       throw new ValidationError(`Expected InputModel with required properties`, null, null);
