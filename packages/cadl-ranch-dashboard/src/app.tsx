cf23e8af0fe4e94bcfc20166e8bbbc1fe0b3c00f
Timothee Guerin <tiguerin@microsoft.com>
2022-09-21 14:41:36 -0700

    Basic tableg

--- packages/cadl-ranch-dashboard/src/app.tsx
+++ packages/cadl-ranch-dashboard/src/app.tsx
@@ -1,9 +1,21 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import { getManifest, ScenarioManifest } from "./apis.js";
+import { Dashboard } from "./dashboard.js";
+import { useEffectAsync } from "./utils.js";
 
 export const App: FunctionComponent = () => {
+  const [manifest, setManifest] = useState<ScenarioManifest | undefined>(undefined);
+
+  useEffectAsync(async () => {
+    const manifest = await getManifest();
+    if (manifest) {
+      setManifest(() => manifest);
+    }
+  }, []);
   return (
     <div>
       <h1>Cadl Ranch Coverage Dashboard</h1>
+      {manifest ? <Dashboard manifest={manifest}></Dashboard> : "Loading"}
     </div>
   );
 };
