2abacc0313d191a32e6db266adb7cb0c0fe869f4
Timothee Guerin <tiguerin@microsoft.com>
2022-09-28 17:06:38 -0700

    Rename

--- packages/cadl-ranch-specs/http/models/usage/mockapi.ts
+++ packages/cadl-ranch-specs/http/models/usage/mockapi.ts
@@ -5,20 +5,20 @@ export const Scenarios: Record<string, ScenarioMockApi> = {};
 
 const body = { requiredProp: "example-value" };
 
-Scenarios.Records_Usage_input = passOnSuccess(
+Scenarios.Models_Usage_input = passOnSuccess(
   mockapi.post("/models/usage/input", (req) => {
     req.expect.bodyEquals(body);
     return { status: 204 };
   }),
 );
 
-Scenarios.Records_Usage_output = passOnSuccess(
+Scenarios.Models_Usage_output = passOnSuccess(
   mockapi.post("/models/usage/output", (req) => {
     return { status: 202, body: json(body) };
   }),
 );
 
-Scenarios.Records_Usage_inputAndOutput = passOnSuccess(
+Scenarios.Models_Usage_inputAndOutput = passOnSuccess(
   mockapi.post("/models/usage/input-output", (req) => {
     req.expect.bodyEquals(body);
     return { status: 202, body: json(body) };
