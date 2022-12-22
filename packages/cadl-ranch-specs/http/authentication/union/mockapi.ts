c7ac72b9740b26d230b09701ebcf1c28a3e27fee
Laurent Mazuel <laurent.mazuel@gmail.com>
2022-12-21 17:23:09 -0800

    Rename to Union

--- packages/cadl-ranch-specs/http/authentication/union/mockapi.ts
+++ packages/cadl-ranch-specs/http/authentication/union/mockapi.ts
@@ -3,15 +3,15 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Authentication_Multiple_validKey = passOnSuccess(
-  mockapi.get("/authentication/multiple/validkey", (req) => {
+Scenarios.Authentication_Union_validKey = passOnSuccess(
+  mockapi.get("/authentication/union/validkey", (req) => {
     req.expect.containsHeader("x-ms-api-key", "valid-key");
     return { status: 204 };
   }),
 );
 
-Scenarios.Authentication_Multiple_validToken = passOnSuccess(
-  mockapi.get("/authentication/multiple/validtoken", (req) => {
+Scenarios.Authentication_Union_validToken = passOnSuccess(
+  mockapi.get("/authentication/union/validtoken", (req) => {
     req.expect.containsHeader("authorization", "Bearer https://security.microsoft.com/.default");
     return { status: 204 };
   }),
