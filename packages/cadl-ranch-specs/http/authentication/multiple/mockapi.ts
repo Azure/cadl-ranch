d881746b06f32740a9e1fe346166987b1df2d1f6
Laurent Mazuel <laurent.mazuel@gmail.com>
2022-12-21 17:20:59 -0800

    Scenario match

--- packages/cadl-ranch-specs/http/authentication/multiple/mockapi.ts
+++ packages/cadl-ranch-specs/http/authentication/multiple/mockapi.ts
@@ -3,14 +3,14 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Authentication_Multiple_validkey = passOnSuccess(
+Scenarios.Authentication_Multiple_validKey = passOnSuccess(
   mockapi.get("/authentication/multiple/validkey", (req) => {
     req.expect.containsHeader("x-ms-api-key", "valid-key");
     return { status: 204 };
   }),
 );
 
-Scenarios.Authentication_Multiple_validtoken = passOnSuccess(
+Scenarios.Authentication_Multiple_validToken = passOnSuccess(
   mockapi.get("/authentication/multiple/validtoken", (req) => {
     req.expect.containsHeader("authorization", "Bearer https://security.microsoft.com/.default");
     return { status: 204 };
