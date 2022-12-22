1fd3a5e76beb9b511d92f869d7d942714d1e64df
Laurent Mazuel <laurent.mazuel@gmail.com>
2022-12-21 16:50:03 -0800

    Add multiple Auth tests in CADL-Ranch

---
+++ packages/cadl-ranch-specs/http/authentication/multiple/mockapi.ts
@@ -0,0 +1,18 @@
+import { passOnSuccess, passOnCode, mockapi, json } from "@azure-tools/cadl-ranch-api";
+import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
+
+export const Scenarios: Record<string, ScenarioMockApi> = {};
+
+Scenarios.Authentication_Multiple_validkey = passOnSuccess(
+  mockapi.get("/authentication/multiple/validkey", (req) => {
+    req.expect.containsHeader("x-ms-api-key", "valid-key");
+    return { status: 204 };
+  }),
+);
+
+Scenarios.Authentication_multiple_validtoken = passOnSuccess(
+  mockapi.get("/authentication/multiple/validtoken", (req) => {
+    req.expect.containsHeader("authorization", "Bearer https://security.microsoft.com/.default");
+    return { status: 204 };
+  }),
+);
