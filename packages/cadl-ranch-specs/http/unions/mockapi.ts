9e9d9d165283bedd931a01ce8fe4549268263762
Changlong Liu <changlong.liu@microsoft.com>
2023-02-06 17:53:11 +0800

    scenarioname

--- packages/cadl-ranch-specs/http/unions/mockapi.ts
+++ packages/cadl-ranch-specs/http/unions/mockapi.ts
@@ -3,28 +3,28 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Union_sendInt = passOnSuccess(
+Scenarios.Unions_sendInt = passOnSuccess(
   mockapi.post("/unions/int", (req) => {
     req.expect.bodyEquals({ "simpleUnion": 1 });
     return { status: 200 };
   }),
 );
 
-Scenarios.Union_sendIntArray = passOnSuccess(
+Scenarios.Unions_sendIntArray = passOnSuccess(
   mockapi.post("/unions/int-array", (req) => {
     req.expect.bodyEquals({ "simpleUnion": [1, 2] });
     return { status: 200 };
   }),
 );
 
-Scenarios.Union_sendInstallation1 = passOnSuccess(
+Scenarios.Unions_sendInstallation1 = passOnSuccess(
   mockapi.post("/unions/installation1", (req) => {
     req.expect.bodyEquals({ "namedUnion": { "name": "installation1", "prop1": 1 } });
     return { status: 200 };
   }),
 );
 
-Scenarios.Union_sendInstallation2 = passOnSuccess(
+Scenarios.Unions_sendInstallation2 = passOnSuccess(
   mockapi.post("/unions/installation2", (req) => {
     req.expect.bodyEquals({ "namedUnion": { "name": "installation2", "prop2": 2 } });
     return { status: 200 };
