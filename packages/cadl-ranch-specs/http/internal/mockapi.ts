28cd707f394ee0b1a2bcc42d1b3c73a4a13a77ed
tadelesh <tadelesh.shi@live.cn>
2023-03-22 16:27:37 +0800

    update internal testcase

--- packages/cadl-ranch-specs/http/internal/mockapi.ts
+++ packages/cadl-ranch-specs/http/internal/mockapi.ts
@@ -14,3 +14,18 @@ Scenarios.Internal_getInternal = passOnSuccess(
     };
   }),
 );
+
+Scenarios.Internal_postInternal = passOnSuccess(
+  mockapi.post("/internal/postInternal", (req) => {
+    if (req.body["id"] !== 1) {
+      throw new ValidationError("Should submit body id", "1", undefined);
+    }
+    if (!("name" in req.body)) {
+      throw new ValidationError("Should submit body name", "any string", undefined);
+    }
+    return {
+      status: 200,
+      body: json({ id: 1, name: req.body["name"] }),
+    };
+  }),
+);
