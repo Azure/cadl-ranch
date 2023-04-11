6440f07a841c916a2cf90e887e86730b637e7c9a
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2023-04-11 11:32:55 -0700

    add tests for client path parameters (#167)

--- packages/cadl-ranch-specs/http/server/path/single/mockapi.ts
+++ packages/cadl-ranch-specs/http/server/path/single/mockapi.ts
@@ -3,8 +3,8 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Server_Parameterized_myOp = passOnSuccess(
-  mockapi.head("/server/parameterized/myOp", (req) => {
+Scenarios.Server_Path_Single_myOp = passOnSuccess(
+  mockapi.head("/server/path/single/myOp", (req) => {
     return { status: 200 };
   }),
 );
