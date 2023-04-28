f42c14b18e4792e1534ed28c3a309cb76c67d74d
Timothee Guerin <tiguerin@microsoft.com>
2023-04-28 13:27:05 -0700

    Auto release

--- eng/publish.mjs
+++ eng/publish.mjs
@@ -7,3 +7,9 @@ execSync(`git config --global user.name "Auto Changeset Bot"`);
 execSync(`pnpm changeset version`);
 execSync(`git commit -am "Bump versions"`);
 execSync(`git push origin HEAD:${branchName} --force`);
+
+console.log();
+console.log("-".repeat(160));
+console.log("|  Link to create the PR");
+console.log(`|  https://github.com/Azure/cadl-ranch/pull/new/${branchName}  `);
+console.log("-".repeat(160));
