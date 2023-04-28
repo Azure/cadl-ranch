c4decea98c7482d744835acfa853d2a3381ce7c2
Timothee Guerin <tiguerin@microsoft.com>
2023-04-28 13:20:41 -0700

    Auto release

--- eng/publish.mjs
+++ eng/publish.mjs
@@ -2,6 +2,8 @@ import { execSync } from "child_process";
 
 const branchName = "publish/auto-release";
 
+execSync(`git config --global user.email "noreply@microsoft.com"`);
+execSync(`git config --global user.name "Auto Changeset Bot"`);
 execSync(`pnpm changeset version`);
 execSync(`git commit -am "Bump versions"`);
 execSync(`git push --set-upstream origin ${branchName} --force`);
