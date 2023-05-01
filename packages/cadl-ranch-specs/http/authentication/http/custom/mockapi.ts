56255b1712d77db954e828e758b95f216f0d9957
iscai-msft <iscai@microsoft.com>
2023-05-01 16:00:44 -0700

    rename basic -> custom

--- packages/cadl-ranch-specs/http/authentication/http/custom/mockapi.ts
+++ packages/cadl-ranch-specs/http/authentication/http/custom/mockapi.ts
@@ -5,13 +5,13 @@ import { getValidAndInvalidScenarios } from "../../commonapi.js";
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
 const validAndInvalidScenarios = getValidAndInvalidScenarios(
-  "http/basic",
+  "http/custom",
   "invalid-api-key",
   function addOptionalParamOldApiVersionNewClientValidate(req: MockRequest): void {
     req.expect.containsHeader("Authorization", "SharedAccessKey valid-key");
   },
 );
 
-Scenarios.Authentication_Http_Basic_valid = validAndInvalidScenarios.valid;
+Scenarios.Authentication_Http_Custom_valid = validAndInvalidScenarios.valid;
 
-Scenarios.Authentication_Http_Basic_invalid = validAndInvalidScenarios.invalid;
+Scenarios.Authentication_Http_Custom_invalid = validAndInvalidScenarios.invalid;
