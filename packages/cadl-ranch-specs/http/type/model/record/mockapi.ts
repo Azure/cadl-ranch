ac725f226c08e436633fd2a4cd62fcb30decf479
msyyc <70930885+msyyc@users.noreply.github.com>
2023-05-22 13:16:29 +0800

    ci

--- packages/cadl-ranch-specs/http/type/model/record/mockapi.ts
+++ packages/cadl-ranch-specs/http/type/model/record/mockapi.ts
@@ -4,14 +4,14 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
 const ModelRecordUnknownBody = { name: "ModelRecordUnknown", prop1: 32, prop2: true, prop3: "abc" };
-Scenarios.Type_Model_Record_postModelRecordUnknown = passOnSuccess(
+Scenarios.Type_Model_RecordTest_RecordUnknown_postModelRecordUnknown = passOnSuccess(
   mockapi.post("/type/model/record/unknown", (req) => {
     req.expect.bodyEquals(ModelRecordUnknownBody);
     return { status: 200 };
   }),
 );
 
-Scenarios.Type_Model_Record_getModelRecordUnknown = passOnSuccess(
+Scenarios.Type_Model_RecordTest_RecordUnknown_getModelRecordUnknown = passOnSuccess(
   mockapi.get("/type/model/record/unknown", (req) => {
     return { status: 200, body: json(ModelRecordUnknownBody) };
   }),
