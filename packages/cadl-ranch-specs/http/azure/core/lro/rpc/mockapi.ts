8e5499f27485f26fa55c9ebe607ef2bd0ecaf4ca
Weidong Xu <weidxu@microsoft.com>
2023-06-07 14:43:29 +0800

    update mockapi

--- packages/cadl-ranch-specs/http/azure/core/lro/rpc/mockapi.ts
+++ packages/cadl-ranch-specs/http/azure/core/lro/rpc/mockapi.ts
@@ -3,13 +3,9 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-const jobInProgress = { jobId: "job1", comment: "async job", status: "InProgress" };
-const jobSucceeded = { jobId: "job1", comment: "async job", status: "Succeeded", results: ["job1 result"] };
+const jobInProgress = { jobId: "job1", comment: "async job", status: "running" };
+const jobSucceeded = { jobId: "job1", comment: "async job", status: "succeeded", results: ["job1 result"] };
 let createPollCount = 0;
-
-const operationInProgress = { operationId: "operation1", status: "InProgress" };
-const operationSucceeded = { operationId: "operation1", status: "Succeeded" };
-let createFinalOnLocationPollCount = 0;
 
 Scenarios.Azure_Core_Lro_Rpc_SamePollResult = passOnSuccess([
   mockapi.post("/azure/core/lro/rpc/same-poll-result/jobs", (req) => {
@@ -19,36 +15,11 @@ Scenarios.Azure_Core_Lro_Rpc_SamePollResult = passOnSuccess([
     return {
       status: 202,
       headers: { "operation-location": `${req.baseUrl}/azure/core/lro/rpc/same-poll-result/jobs/job1` },
-      body: json(jobInProgress),
     };
   }),
   mockapi.get("/azure/core/lro/rpc/same-poll-result/jobs/job1", (req) => {
     const response = createPollCount > 0 ? jobSucceeded : jobInProgress;
     createPollCount += 1;
     return { status: 200, body: json(response) };
-  }),
-]);
-
-Scenarios.Azure_Core_Lro_Rpc_DifferentPollResult = passOnSuccess([
-  mockapi.post("/azure/core/lro/rpc/different-poll-result/jobs", (req) => {
-    req.expect.containsQueryParam("api-version", "2022-12-01-preview");
-    req.expect.bodyEquals({ comment: "async job" });
-    createFinalOnLocationPollCount = 0;
-    return {
-      status: 202,
-      headers: {
-        "operation-location": `${req.baseUrl}/azure/core/lro/rpc/different-poll-result/jobs/operations/operation1`,
-        "location": `${req.baseUrl}/azure/core/lro/rpc/different-poll-result/jobs/job1`,
-      },
-      body: json(operationInProgress),
-    };
-  }),
-  mockapi.get("/azure/core/lro/rpc/different-poll-result/jobs/operations/operation1", (req) => {
-    const response = createFinalOnLocationPollCount > 0 ? operationSucceeded : operationInProgress;
-    createFinalOnLocationPollCount += 1;
-    return { status: 200, body: json(response) };
-  }),
-  mockapi.get("/azure/core/lro/rpc/different-poll-result/jobs/job1", (req) => {
-    return { status: 200, body: json(jobSucceeded) };
   }),
 ]);
