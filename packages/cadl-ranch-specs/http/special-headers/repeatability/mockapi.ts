6db41cb46ac7fed42fc5a169ef082904ea9b96ee
tadelesh <tadelesh.shi@live.cn>
2023-06-07 10:45:39 +0800

    refine mockapi

--- packages/cadl-ranch-specs/http/special-headers/repeatability/mockapi.ts
+++ packages/cadl-ranch-specs/http/special-headers/repeatability/mockapi.ts
@@ -1,23 +1,17 @@
-import { passOnSuccess, ScenarioMockApi, mockapi, ValidationError } from "@azure-tools/cadl-ranch-api";
+import { passOnSuccess, ScenarioMockApi, mockapi, ValidationError, validateValueFormat } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
 Scenarios.SpecialHeaders_Repeatability_immediateSuccess = passOnSuccess(
   mockapi.post("/special-headers/repeatability/immediateSuccess", (req) => {
     if (!("repeatability-request-id" in req.headers)) {
-      throw new ValidationError(
-        "Repeatability-Request-ID is missing",
-        "A UUID string",
-        req.headers["Repeatability-Request-ID"],
-      );
+      throw new ValidationError("Repeatability-Request-ID is missing", "A UUID string", undefined);
     }
     if (!("repeatability-first-sent" in req.headers)) {
-      throw new ValidationError(
-        "Repeatability-First-Sent is missing",
-        "A date-time in headers format",
-        req.headers["Repeatability-First-Sent"],
-      );
+      throw new ValidationError("Repeatability-First-Sent is missing", "A date-time in headers format", undefined);
     }
+    validateValueFormat(req.headers["repeatability-request-id"], "uuid");
+    validateValueFormat(req.headers["repeatability-first-sent"], "rfc7123");
     return {
       status: 204,
       headers: {
