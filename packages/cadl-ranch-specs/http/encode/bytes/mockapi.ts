00883389f7f0549aeb32ea2cc0c0e5a13c91311e
tadelesh <tadelesh.shi@live.cn>
2023-06-14 14:36:42 +0800

    refine with review

--- packages/cadl-ranch-specs/http/encode/bytes/mockapi.ts
+++ packages/cadl-ranch-specs/http/encode/bytes/mockapi.ts
@@ -3,25 +3,25 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Encode_Bytes_query = passOnSuccess(
-  mockapi.get("/encode/bytes/query", (req) => {
+Scenarios.Encode_Bytes_Query_get = passOnSuccess(
+  mockapi.get("/encode/bytes/query/get", (req) => {
     req.expect.containsQueryParam("default", "dGVzdA==");
     req.expect.containsQueryParam("base64", "dGVzdA==");
     req.expect.containsQueryParam("base64url", "dGVzdA");
-    req.expect.containsQueryParam("base64-array", ["dGVzdA==", "dGVzdA=="], "csv");
+    req.expect.containsQueryParam("base64url-array", ["dGVzdA", "dGVzdA"], "csv");
     return {
       status: 204,
     };
   }),
 );
 
-Scenarios.Encode_Bytes_property = passOnSuccess(
-  mockapi.post("/encode/bytes/property", (req) => {
+Scenarios.Encode_Bytes_Property_post = passOnSuccess(
+  mockapi.post("/encode/bytes/property/post", (req) => {
     const body = {
       default: "dGVzdA==",
       base64: "dGVzdA==",
       base64url: "dGVzdA",
-      base64Array: ["dGVzdA==", "dGVzdA=="],
+      base64urlArray: ["dGVzdA", "dGVzdA"],
     };
     req.expect.coercedBodyEquals(body);
     return {
@@ -31,12 +31,12 @@ Scenarios.Encode_Bytes_property = passOnSuccess(
   }),
 );
 
-Scenarios.Encode_Bytes_header = passOnSuccess(
-  mockapi.get("/encode/bytes/header", (req) => {
+Scenarios.Encode_Bytes_Header_get = passOnSuccess(
+  mockapi.get("/encode/bytes/header/get", (req) => {
     req.expect.containsHeader("default", "dGVzdA==");
     req.expect.containsHeader("base64", "dGVzdA==");
     req.expect.containsHeader("base64url", "dGVzdA");
-    req.expect.containsHeader("base64-array", "dGVzdA==,dGVzdA==");
+    req.expect.containsHeader("base64url-array", "dGVzdA,dGVzdA");
     return {
       status: 204,
     };
--- packages/cadl-ranch-specs/http/encode/datetime/mockapi.ts
+++ packages/cadl-ranch-specs/http/encode/datetime/mockapi.ts
@@ -3,27 +3,27 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Encode_Datetime_query = passOnSuccess(
-  mockapi.get("/encode/datetime/query", (req) => {
+Scenarios.Encode_Datetime_Query_get = passOnSuccess(
+  mockapi.get("/encode/datetime/query/get", (req) => {
     req.expect.containsQueryParam("default", "2022-08-26T18:38:00.000Z");
     req.expect.containsQueryParam("rfc3339", "2022-08-26T18:38:00.000Z");
     req.expect.containsQueryParam("rfc7231", "Fri, 26 Aug 2022 14:38:00 GMT");
     req.expect.containsQueryParam("unix-timestamp", "1686566864");
-    req.expect.containsQueryParam("rfc3339-array", ["2022-08-26T18:38:00.000Z", "2022-09-26T18:38:00.000Z"], "csv");
+    req.expect.containsQueryParam("rfc7231-array", ["Fri, 26 Aug 2022 14:38:00 GMT", "Fri, 26 Aug 2022 16:38:00 GMT"], "csv");
     return {
       status: 204,
     };
   }),
 );
 
-Scenarios.Encode_Datetime_property = passOnSuccess(
-  mockapi.post("/encode/datetime/property", (req) => {
+Scenarios.Encode_Datetime_Property_post = passOnSuccess(
+  mockapi.post("/encode/datetime/property/post", (req) => {
     const body = {
       default: "2022-08-26T18:38:00.000Z",
       rfc3339: "2022-08-26T18:38:00.000Z",
       rfc7231: "Fri, 26 Aug 2022 14:38:00 GMT",
       unixTimestamp: 1686566864,
-      rfc3339Array: ["2022-08-26T18:38:00.000Z", "2022-09-26T18:38:00.000Z"],
+      rfc7231Array: ["Fri, 26 Aug 2022 14:38:00 GMT", "Fri, 26 Aug 2022 16:38:00 GMT"],
     };
     req.expect.coercedBodyEquals(body);
     return {
@@ -33,13 +33,13 @@ Scenarios.Encode_Datetime_property = passOnSuccess(
   }),
 );
 
-Scenarios.Encode_Datetime_header = passOnSuccess(
-  mockapi.get("/encode/datetime/header", (req) => {
+Scenarios.Encode_Datetime_Header_get = passOnSuccess(
+  mockapi.get("/encode/datetime/header/get", (req) => {
     req.expect.containsHeader("default", "Fri, 26 Aug 2022 14:38:00 GMT");
     req.expect.containsHeader("rfc3339", "2022-08-26T18:38:00.000Z");
     req.expect.containsHeader("rfc7231", "Fri, 26 Aug 2022 14:38:00 GMT");
     req.expect.containsHeader("unix-timestamp", "1686566864");
-    req.expect.containsHeader("rfc3339-array", "2022-08-26T18:38:00.000Z,2022-09-26T18:38:00.000Z");
+    req.expect.containsHeader("rfc7231-array", "Fri, 26 Aug 2022 14:38:00 GMT,Fri, 26 Aug 2022 16:38:00 GMT");
     return {
       status: 204,
     };
