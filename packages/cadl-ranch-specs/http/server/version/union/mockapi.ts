db6934636c68264553f55f9916c942786a344deb
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-06-20 16:41:28 +0800

    Fix

--- packages/cadl-ranch-specs/http/server/version/union/mockapi.ts
+++ packages/cadl-ranch-specs/http/server/version/union/mockapi.ts
@@ -17,7 +17,7 @@ Scenarios.Server_Version_Union_customerDefinedWithDefault = passOnSuccess(
 );
 
 Scenarios.Server_Version_Union_templateDefined = passOnSuccess(
-  mockapi.head("/server/version/union/Pet/dog", (req) => {
+  mockapi.delete("/server/version/union/Pet/dog", (req) => {
     req.expect.containsQueryParam("api-version", "1.0.0");
     return { status: 204 };
   }),
