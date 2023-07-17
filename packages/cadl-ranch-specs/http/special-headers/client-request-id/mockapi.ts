62df45ce0c3c90c235b7730b16a4b8bd64418d7c
tadelesh <tadelesh.shi@live.cn>
2023-07-17 10:11:15 +0800

    fix ci

--- packages/cadl-ranch-specs/http/special-headers/client-request-id/mockapi.ts
+++ packages/cadl-ranch-specs/http/special-headers/client-request-id/mockapi.ts
@@ -2,7 +2,7 @@ import { passOnSuccess, ScenarioMockApi, mockapi, validateValueFormat } from "@a
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.SpecialHeaders_ClientRequestId_get = passOnSuccess(
+Scenarios.SpecialHeaders_ClientRequestId = passOnSuccess(
   mockapi.get("/special-headers/client-request-id", (req) => {
     validateValueFormat(req.headers["client-request-id"], "uuid");
     return {
