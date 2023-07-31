8c6f168f40741bc4751e86d4be07dcd4147f8234
qiaozha <qiaozha@microsoft.com>
2023-08-01 07:54:12 +0800

    use 204 and remove unused mock apis and fix some typo

--- packages/cadl-ranch-specs/http/client/structure/default/mockapi.ts
+++ packages/cadl-ranch-specs/http/client/structure/default/mockapi.ts
@@ -4,21 +4,21 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 Scenarios.Client_Structure_Service = passOnSuccess([
   mockapi.patch("/client/structure/default/one", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
   mockapi.patch("/client/structure/default/two", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
   mockapi.patch("/client/structure/default/three", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
   mockapi.patch("/client/structure/default/four", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
   mockapi.patch("/client/structure/default/five", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
   mockapi.patch("/client/structure/default/six", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
 ]);
--- packages/cadl-ranch-specs/http/client/structure/two-operation-group/mockapi.ts
+++ packages/cadl-ranch-specs/http/client/structure/two-operation-group/mockapi.ts
@@ -4,21 +4,21 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 Scenarios.Client_Structure_TwoOperationGroup = passOnSuccess([
   mockapi.patch("/client/structure/two-operation-group/one", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
   mockapi.patch("/client/structure/two-operation-group/two", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
   mockapi.patch("/client/structure/two-operation-group/three", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
   mockapi.patch("/client/structure/two-operation-group/four", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
   mockapi.patch("/client/structure/two-operation-group/five", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
   mockapi.patch("/client/structure/two-operation-group/six", (req) => {
-    return { status: 200 };
+    return { status: 204 };
   }),
 ]);
