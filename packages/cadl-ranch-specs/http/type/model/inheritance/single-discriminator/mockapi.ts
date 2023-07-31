c9181201fb32424eda6cd59ea8b63ca71bbf3b3e
Mingzhe Huang <archerzz@users.noreply.github.com>
2023-07-31 11:05:57 +0800

    feat(specs): refactor and add new tests to model inheritance (#381)
    - split inheritance test cases into 4 sub test cases:
      - `not-discriminated`: model hierarchy without discriminator property
      - `single-discriminator`: model hierarchy with one discriminator property
      -  `nested-discriminator`: model hierarchy with multiple discriminator properties on multiple levels
      - `enum-discriminator`: discriminator property is of fixed and extensible enum types
    - and test cases for single discriminator and enum discriminator
    
    resolve #379

---
+++ packages/cadl-ranch-specs/http/type/model/inheritance/single-discriminator/mockapi.ts
@@ -0,0 +1,72 @@
+import { passOnSuccess, mockapi, json } from "@azure-tools/cadl-ranch-api";
+import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
+
+export const Scenarios: Record<string, ScenarioMockApi> = {};
+
+const validPolymorphicBody = {
+  wingspan: 1,
+  kind: "sparrow",
+};
+Scenarios.Type_Model_Inheritance_SingleDiscriminator_getModel = passOnSuccess(
+  mockapi.get("/type/model/inheritance/single-discriminator/model", (req) => {
+    return { status: 200, body: json(validPolymorphicBody) };
+  }),
+);
+
+Scenarios.Type_Model_Inheritance_SingleDiscriminator_putModel = passOnSuccess(
+  mockapi.put("/type/model/inheritance/single-discriminator/model", (req) => {
+    req.expect.bodyEquals(validPolymorphicBody);
+    return { status: 204 };
+  }),
+);
+
+const validRecursiveBody = {
+  wingspan: 5,
+  kind: "eagle",
+  partner: {
+    wingspan: 2,
+    kind: "goose",
+  },
+  friends: [
+    {
+      wingspan: 2,
+      kind: "seagull",
+    },
+  ],
+  hate: {
+    key3: {
+      wingspan: 1,
+      kind: "sparrow",
+    },
+  },
+};
+Scenarios.Type_Model_Inheritance_SingleDiscriminator_getRecursiveModel = passOnSuccess(
+  mockapi.get("/type/model/inheritance/single-discriminator/recursivemodel", (req) => {
+    return { status: 200, body: json(validRecursiveBody) };
+  }),
+);
+
+Scenarios.Type_Model_Inheritance_SingleDiscriminator_putRecursiveModel = passOnSuccess(
+  mockapi.put("/type/model/inheritance/single-discriminator/recursivemodel", (req) => {
+    req.expect.bodyEquals(validRecursiveBody);
+    return { status: 204 };
+  }),
+);
+
+Scenarios.Type_Model_Inheritance_SingleDiscriminator_getMissingDiscriminator = passOnSuccess(
+  mockapi.get("/type/model/inheritance/single-discriminator/missingdiscriminator", (req) => {
+    return { status: 200, body: json({ wingspan: 1 }) };
+  }),
+);
+
+Scenarios.Type_Model_Inheritance_SingleDiscriminator_getWrongDiscriminator = passOnSuccess(
+  mockapi.get("/type/model/inheritance/single-discriminator/wrongdiscriminator", (req) => {
+    return { status: 200, body: json({ wingspan: 1, kind: "wrongKind" }) };
+  }),
+);
+
+Scenarios.Type_Model_Inheritance_SingleDiscriminator_getLegacyModel = passOnSuccess(
+  mockapi.get("/type/model/inheritance/single-discriminator/legacy-model", (req) => {
+    return { status: 200, body: json({ size: 20, kind: "t-rex" }) };
+  }),
+);
