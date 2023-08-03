a2898fa8e6e0f36f3ea942d5a78e23f2965e9e39
Patrick Hallisey <pahallis@microsoft.com>
2023-08-03 12:59:09 -0700

    Add tests for version ranges

--- packages/typespec-bump-deps/src/cli/typespec-bump-deps.ts
+++ packages/typespec-bump-deps/src/cli/typespec-bump-deps.ts
@@ -13,29 +13,15 @@ const knownPackages = [
   "@typespec/eslint-config-typespec",
 ];
 
-function isComplex(range: semver.Range): boolean {
-  /* a complex range has multiple segments,
-   *   ">=0.1.2 || 1.2.3"
-   *  or multiple comparators,
-   *   ">=0.1.2 <1.2.3"
-   *  or an operator
-   *   ">=1.2.3"
-   *
-   *  a simple range has a single segment with a single comparator and no operator
-   *   "1.2.3"
-   */
-  return range?.set.length > 0 && (range.set.length > 1 || range.set[0].length > 1 || range.set[0][0].operator !== "");
-}
+const depTypes = ["dependencies", "devDependencies", "peerDependencies"];
 
-function getVersionRange(version: string): string {
+export function getVersionRange(version: string): string {
   const parsed = semver.parse(version);
   if (!parsed) {
     throw new Error(`Unable to parse version ${version}`);
   }
 
-  const preReleaseSuffix = parsed.prerelease.length > 0 ? `-${parsed.prerelease[0]}` : "";
-
-  return `>=${parsed.major}.${parsed.minor}.0${preReleaseSuffix} <${parsed.major}.${parsed.minor}.0`;
+  return `>=${parsed.major}.${parsed.minor}.0-0 <${parsed.major}.${parsed.minor}.0`;
 }
 
 async function getKnownPackageVersion(packageName: string): Promise<string> {
@@ -66,9 +52,7 @@ export async function main() {
   const packageToVersionRecord = Object.fromEntries(
     await Promise.all(knownPackages.map(async (x) => [x, await getKnownPackageVersion(x)])),
   );
-  const packageToVersionRange = Object.fromEntries(
-    knownPackages.map((x) => [x, getVersionRange(packageToVersionRecord[x])]),
-  );
+
   // eslint-disable-next-line no-console
   console.log("The following is a mapping between packages and the versions we will update them to");
   // eslint-disable-next-line no-console
@@ -78,14 +62,7 @@ export async function main() {
     const content = await readFile(packageJsonPath);
     const packageJson = JSON.parse(content.toString());
 
-    updatePackageJson(
-      packageJson,
-      packageToVersionRecord,
-      packageToVersionRange,
-      keepRanges,
-      addNpmOverrides,
-      addRushOverrides,
-    );
+    updatePackageJson(packageJson, packageToVersionRecord, keepRanges, addNpmOverrides, addRushOverrides);
 
     // eslint-disable-next-line no-console
     console.log(`Updated ${packageJsonPath}`);
@@ -95,42 +72,41 @@ export async function main() {
 
 export function updatePackageJson(
   packageJson: any,
-  packageToVersionRecord: any,
-  packageToVersionRange: { [k: string]: string },
+  packageToVersionRecord: { [key: string]: string },
   keepRanges: boolean,
   addNpmOverrides: boolean,
   addRushOverrides: boolean,
 ) {
-  const depTypes = ["dependencies", "devDependencies", "peerDependencies"];
+  const packageToVersionRange = Object.fromEntries(
+    Object.entries(packageToVersionRecord).map(([key, value]) => [key, getVersionRange(value)]),
+  );
 
-  for (const packageName of knownPackages) {
-    const newVersion = packageToVersionRecord[packageName];
+  let overridesType: string | undefined = undefined;
+  if (addNpmOverrides) {
+    overridesType = "overrides";
+  } else if (addRushOverrides) {
+    overridesType = "globalOverrides";
+  }
 
-    if (keepRanges && packageJson.peerDependencies && packageJson.devDependencies) {
-      const peerDependency = packageJson.peerDependencies[packageName];
-      const peerRange = peerDependency ? new semver.Range(peerDependency) : undefined;
-      const devDependency = packageJson.devDependencies[packageName];
-      const dependency = packageJson.dependencies[packageName];
+  for (const [packageName, version] of Object.entries(packageToVersionRecord)) {
+    if (keepRanges) {
+      const peerDependency = packageJson.peerDependencies ? packageJson.peerDependencies[packageName] : undefined;
 
-      if (peerRange && devDependency && !dependency) {
-        // we don't do range based peerDependencies if there's a dependencies entry for the same package
-        if (!semver.satisfies(newVersion, peerRange)) {
-          // if the new version doesn't satisfy the existing range, we add the new range
-          if (isComplex(peerRange)) {
-            // complex ranges get the new range appended
-            // ">= 0.3.0 <1.0.0" => ">= 0.3.0 <1.0.0 || >= 0.4.0-dev <0.4.0"
-            packageJson.peerDependencies[packageName] += ` || ${packageToVersionRange[packageName]}`;
-          } else {
-            // simple ranges get replaced with the new range
-            // "1.2.0" => ">= 0.4.0-dev <0.4.0"
-            packageJson.peerDependencies[packageName] = packageToVersionRange[packageName];
-          }
-        }
-        packageJson.devDependencies[packageName] = newVersion;
+      if (peerDependency) {
         if (packageJson.dependencies && packageJson.dependencies[packageName]) {
-          delete packageJson.dependencies[packageName];
+          throw new Error(`${packageName} is both a dependency and peerDependency`);
         }
-        return;
+
+        // we don't do range based peerDependencies if there's a dependencies entry for the same package
+        if (!semver.satisfies(version, peerDependency)) {
+          // if the new version doesn't satisfy the existing range, we append the new range
+          // ">= 0.3.0 <1.0.0" becomes ">= 0.3.0 <1.0.0 || >= 0.4.0-dev <0.4.0"
+          // "1.2.0" becomes "1.2.0 || >= 0.4.0-dev <0.4.0"
+          packageJson.peerDependencies[packageName] += ` || ${packageToVersionRange[packageName]}`;
+        }
+
+        packageJson.devDependencies[packageName] = version;
+        continue;
       }
     }
 
@@ -138,25 +114,13 @@ export function updatePackageJson(
       const deps = packageJson[depType];
 
       if (deps && deps[packageName]) {
-        deps[packageName] = newVersion;
+        deps[packageName] = version;
       }
     }
-  }
 
-  // add/merge package versions into "overrides" or "globalOverrides"
-  let overridesType: string | undefined = undefined;
-  if (addNpmOverrides) {
-    overridesType = "overrides";
-  } else if (addRushOverrides) {
-    overridesType = "globalOverrides";
-  }
-  if (overridesType) {
-    let deps = packageJson[overridesType];
-    if (deps === undefined) {
-      deps = {};
-      packageJson[overridesType] = deps;
-    }
-    for (const [packageName, version] of Object.entries(packageToVersionRecord)) {
+    // add/merge package versions into "overrides" or "globalOverrides"
+    if (overridesType) {
+      const deps = (packageJson[overridesType] ??= {});
       deps[packageName] = version;
     }
   }
