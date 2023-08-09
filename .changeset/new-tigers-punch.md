c15099543c799475a66c4507c5d95e58a97850a7
FAREAST\chunyu <chunyu@microsoft.com>
2023-08-09 15:44:26 +0800

    add change log

---
+++ .changeset/new-tigers-punch.md
@@ -0,0 +1,5 @@
+---
+"@azure-tools/cadl-ranch-specs": minor
+---
+
+add two new test cases for head operation in http-method/head
--- packages/cadl-ranch-specs/http/http-method/head/mockapi.ts
+++ packages/cadl-ranch-specs/http/http-method/head/mockapi.ts
@@ -3,13 +3,13 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.HttpMethod_Head204 = passOnSuccess(
+Scenarios.HttpMethod_Head_head204 = passOnSuccess(
   mockapi.head("/http-method/head/204/valid", (req) => {
     return { status: 204 };
   }),
 );
 
-Scenarios.HttpMethod_Head404 = passOnSuccess(
+Scenarios.HttpMethod_Head_head404 = passOnSuccess(
   mockapi.head("/http-method/head/404/invalid", (req) => {
     return {
       status: 404,
