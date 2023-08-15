fa770b25cf1bf8b58ec2294d664e3b6c3171b434
Arcturus Zhang <dapzhang@microsoft.com>
2023-08-15 16:56:23 +0800

    add union in response cases

--- packages/cadl-ranch-specs/http/type/union/mockapi.ts
+++ packages/cadl-ranch-specs/http/type/union/mockapi.ts
@@ -1,4 +1,4 @@
-import { passOnSuccess, mockapi } from "@azure-tools/cadl-ranch-api";
+import { passOnSuccess, mockapi, json } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
@@ -28,5 +28,53 @@ Scenarios.Type_Union_sendSecondNamedUnionValue = passOnSuccess(
   mockapi.post("/type/union/model2", (req) => {
     req.expect.bodyEquals({ namedUnion: { name: "model2", prop2: 2 } });
     return { status: 200 };
+  }),
+);
+
+Scenarios.Type_Union_receiveString = passOnSuccess(
+  mockapi.post("/type/union/receive/string", (req) => {
+    req.expect.bodyEquals({ kind: "string" });
+    return {
+      status: 200,
+      body: json({
+        simpleUnion: "string",
+      }),
+    };
+  }),
+);
+
+Scenarios.Type_Union_receiveIntArray = passOnSuccess(
+  mockapi.post("/type/union/receive/int-array", (req) => {
+    req.expect.bodyEquals({ kind: "int-array" });
+    return {
+      status: 200,
+      body: json({
+        simpleUnion: [1, 2],
+      }),
+    };
+  }),
+);
+
+Scenarios.Type_Union_receiveFirstNamedUnionValue = passOnSuccess(
+  mockapi.post("/type/union/receive/model1", (req) => {
+    req.expect.bodyEquals({ kind: "model1" });
+    return {
+      status: 200,
+      body: json({
+        namedUnion: { name: "model1", prop1: 1 },
+      }),
+    };
+  }),
+);
+
+Scenarios.Type_Union_receiveSecondNamedUnionValue = passOnSuccess(
+  mockapi.post("/type/union/receive/model2", (req) => {
+    req.expect.bodyEquals({ kind: "model2" });
+    return {
+      status: 200,
+      body: json({
+        namedUnion: { name: "model2", prop2: 2 },
+      }),
+    };
   }),
 );
