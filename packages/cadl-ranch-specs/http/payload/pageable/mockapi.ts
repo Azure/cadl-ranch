f79dfcba798c2c47a9b3c8c2775f512449aff780
Weidong Xu <weidxu@microsoft.com>
2023-08-21 16:52:57 +0800

    fix

--- packages/cadl-ranch-specs/http/payload/pageable/mockapi.ts
+++ packages/cadl-ranch-specs/http/payload/pageable/mockapi.ts
@@ -1,4 +1,4 @@
-import { mockapi, json, withKeys } from "@azure-tools/cadl-ranch-api";
+import { mockapi, json, withKeys, ValidationError } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
@@ -7,21 +7,28 @@ Scenarios.Payload_Pageable_list = withKeys(["firstPage", "secondPage"]).pass(
   mockapi.get("/payload/pageable", (req) => {
     req.expect.containsQueryParam("top", "100");
     req.expect.containsQueryParam("maxpagesize", "3");
-    switch (req.query("skip")) {
+    switch (req.query["skip"]) {
       case "5":
         return {
+          pass: "firstPage",
+
           status: 200,
-          body: json({ value: [ {name: "user5"}, {name: "user6"}, {name: "user7"}], nextLink: `${req.baseUrl}//payload/pageable?top=100&skip=8&maxpagesize=3` }),
+          body: json({
+            value: [{ name: "user5" }, { name: "user6" }, { name: "user7" }],
+            nextLink: `${req.baseUrl}/payload/pageable?top=100&skip=8&maxpagesize=3`,
+          }),
         } as const;
-    
+
       case "8":
         return {
+          pass: "secondPage",
+
           status: 200,
-          body: json({ value: [ {name: "user8"}] }),
+          body: json({ value: [{ name: "user8" }] }),
         } as const;
-    
+
       default:
-        throw new ValidationError("Unsupported skip query parameter", `"5" | "8"`, req.query("skip"));
-      }
-  })
+        throw new ValidationError("Unsupported skip query parameter", `"5" | "8"`, req.query["skip"]);
+    }
+  }),
 );
