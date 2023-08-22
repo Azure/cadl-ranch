eb455dc22fa6d078256e63e76fd8b5c94f7461ca
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-08-22 16:11:31 +0800

    Fix

--- packages/cadl-ranch-specs/http/azure/client-generator-core/usage/mockapi.ts
+++ packages/cadl-ranch-specs/http/azure/client-generator-core/usage/mockapi.ts
@@ -1,20 +1,7 @@
-import { passOnSuccess, mockapi, ValidationError, json, MockApi } from "@azure-tools/cadl-ranch-api";
+import { passOnSuccess, mockapi, json } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
-
-function createMockApis(route: string): MockApi {
-  const url = `/azure/client-generator-core/usage/${route}`;
-  return mockapi.get(url, (req) => {
-    if (!("name" in req.query)) {
-      throw new ValidationError("Should submit name query", "any string", undefined);
-    }
-    return {
-      status: 200,
-      body: json({ name: req.query["name"] }),
-    };
-  });
-}
 
 Scenarios.Azure_ClientGenerator_Core_Usage_ModelInOperation = passOnSuccess([
   mockapi.post("/azure/client-generator-core/usage/inputToRoundTrip", (req) => {
