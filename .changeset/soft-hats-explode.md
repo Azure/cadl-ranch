9cfc2828ad12c14693e5363afb46e8a2a960f308
msyyc <70930885+msyyc@users.noreply.github.com>
2023-09-04 13:47:17 +0800

    add test case for etag

---
+++ .changeset/soft-hats-explode.md
@@ -0,0 +1,5 @@
+---
+"@azure-tools/cadl-ranch-specs": minor
+---
+
+Add test case for scenario that only one conditional request header is defined
---
+++ packages/cadl-ranch-specs/http/special-headers/conditional-request/mockapi.ts
@@ -0,0 +1,22 @@
+import { passOnSuccess, mockapi, json, ValidationError, validateValueFormat } from "@azure-tools/cadl-ranch-api";
+import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
+
+export const Scenarios: Record<string, ScenarioMockApi> = {};
+
+Scenarios.SpecialHeaders_ConditionalRequest_postIfMatch = passOnSuccess(
+  mockapi.post("/special-headers/conditional-request/if-match", (req) => {
+    req.expect.containsHeader("if-match", '"valid"');
+    return {
+      status: 204,
+    };
+  }),
+);
+
+Scenarios.SpecialHeaders_ConditionalRequest_postIfNoneMatch = passOnSuccess(
+  mockapi.post("/special-headers/conditional-request/if-none-match", (req) => {
+    req.expect.containsHeader("if-none-match", '"invalid"');
+    return {
+      status: 204,
+    };
+  }),
+);
