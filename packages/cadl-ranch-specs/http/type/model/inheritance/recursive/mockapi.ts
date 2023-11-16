d398e8c6d68290c28cc010168d142bf12f9734b7
msyyc <70930885+msyyc@users.noreply.github.com>
2023-11-16 14:54:49 +0800

    review

--- packages/cadl-ranch-specs/http/type/model/inheritance/recursive/mockapi.ts
+++ packages/cadl-ranch-specs/http/type/model/inheritance/recursive/mockapi.ts
@@ -19,15 +19,15 @@ const body = {
   ],
 };
 
-Scenarios.Type_Model_Inheritance_NestedProperty_put = passOnSuccess(
-  mockapi.put("/type/model/inheritance/nested-property", (req) => {
+Scenarios.Type_Model_Inheritance_Recursive_put = passOnSuccess(
+  mockapi.put("/type/model/inheritance/recursive", (req) => {
     req.expect.bodyEquals(body);
     return { status: 204 };
   }),
 );
 
-Scenarios.Type_Model_Inheritance_NestedProperty_get = passOnSuccess(
-  mockapi.get("/type/model/inheritance/nested-property", (req) => {
+Scenarios.Type_Model_Inheritance_Recursive_get = passOnSuccess(
+  mockapi.get("/type/model/inheritance/recursive", (req) => {
     return { status: 200, body: json(body) };
   }),
 );
