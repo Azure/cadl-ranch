094c4b98746ac41f960948bde793b651fbabb3d5
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-12-18 13:47:59 +0800

    update

--- packages/cadl-ranch-specs/http/server/versions/not-versioned/mockapi.ts
+++ packages/cadl-ranch-specs/http/server/versions/not-versioned/mockapi.ts
@@ -3,8 +3,28 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Server_Versions_NotVersioned = passOnSuccess(
+Scenarios.Server_Versions_NotVersioned_withoutApiVersion = passOnSuccess(
   mockapi.head("/server/versions/notVersioned/withoutApiVersion", (req) => {
+    if (Object.keys(req.originalRequest.query).length > 0) {
+      throw new ValidationError(
+        "Expected no query parameters including api-version",
+        "No query parameters",
+        req.originalRequest.query,
+      );
+    }
+    return { status: 200 };
+  }),
+);
+
+Scenarios.Server_Versions_NotVersioned_withQueryApiVersion = passOnSuccess(
+  mockapi.head("/server/versions/notVersioned/withQueryApiVersion", (req) => {
+    req.expect.containsQueryParam("api-version", "v1.0");
+    return { status: 200 };
+  }),
+);
+
+Scenarios.Server_Versions_NotVersioned_withPathApiVersion = passOnSuccess(
+  mockapi.head("/server/versions/notVersioned/withPathApiVersion/v1.0", (req) => {
     if (Object.keys(req.originalRequest.query).length > 0) {
       throw new ValidationError(
         "Expected no query parameters including api-version",
--- packages/cadl-ranch-specs/http/server/versions/versioned/mockapi.ts
+++ packages/cadl-ranch-specs/http/server/versions/versioned/mockapi.ts
@@ -3,8 +3,8 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Server_Versions_PartiallyVersioned_withoutApiVersion = passOnSuccess(
-  mockapi.head("/server/versions/partiallyVersioned/withoutApiVersion", (req) => {
+Scenarios.Server_Versions_Versioned_withoutApiVersion = passOnSuccess(
+  mockapi.head("/server/versions/versioned/withoutApiVersion", (req) => {
     if (Object.keys(req.originalRequest.query).length > 0) {
       throw new ValidationError(
         "Expected no query parameters including api-version",
@@ -16,9 +16,22 @@ Scenarios.Server_Versions_PartiallyVersioned_withoutApiVersion = passOnSuccess(
   }),
 );
 
-Scenarios.Server_Versions_PartiallyVersioned_withApiVersion = passOnSuccess(
-  mockapi.head("/server/versions/partiallyVersioned/withApiVersion", (req) => {
+Scenarios.Server_Versions_Versioned_withQueryApiVersion = passOnSuccess(
+  mockapi.head("/server/versions/versioned/withQueryApiVersion", (req) => {
     req.expect.containsQueryParam("api-version", "2022-12-01-preview");
+    return { status: 200 };
+  }),
+);
+
+Scenarios.Server_Versions_Versioned_withQueryApiVersion = passOnSuccess(
+  mockapi.head("/server/versions/versioned/withPathApiVersion/2022-12-01-preview", (req) => {
+    if (Object.keys(req.originalRequest.query).length > 0) {
+      throw new ValidationError(
+        "Expected no query parameters including api-version",
+        "No query parameters",
+        req.originalRequest.query,
+      );
+    }
     return { status: 200 };
   }),
 );
