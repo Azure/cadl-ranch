39acca1ba9df8caba81e07859f5d77be42965d1e
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-12-25 11:36:55 +0800

    update

--- packages/cadl-ranch-specs/http/server/versions/not-versioned/mockapi.ts
+++ packages/cadl-ranch-specs/http/server/versions/not-versioned/mockapi.ts
@@ -4,7 +4,7 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
 Scenarios.Server_Versions_NotVersioned_withoutApiVersion = passOnSuccess(
-  mockapi.head("/server/versions/notVersioned/withoutApiVersion", (req) => {
+  mockapi.head("/server/versions/not-versioned/without-api-version", (req) => {
     if (Object.keys(req.query).length > 0) {
       throw new ValidationError("Expected no query parameters including api-version", "No query parameters", req.query);
     }
@@ -13,14 +13,14 @@ Scenarios.Server_Versions_NotVersioned_withoutApiVersion = passOnSuccess(
 );
 
 Scenarios.Server_Versions_NotVersioned_withQueryApiVersion = passOnSuccess(
-  mockapi.head("/server/versions/notVersioned/withQueryApiVersion", (req) => {
+  mockapi.head("/server/versions/not-versioned/with-query-api-version", (req) => {
     req.expect.containsQueryParam("api-version", "v1.0");
     return { status: 200 };
   }),
 );
 
 Scenarios.Server_Versions_NotVersioned_withPathApiVersion = passOnSuccess(
-  mockapi.head("/server/versions/notVersioned/withPathApiVersion/v1.0", (req) => {
+  mockapi.head("/server/versions/not-versioned/with-path-api-version/v1.0", (req) => {
     if (Object.keys(req.query).length > 0) {
       throw new ValidationError("Expected no query parameters including api-version", "No query parameters", req.query);
     }
