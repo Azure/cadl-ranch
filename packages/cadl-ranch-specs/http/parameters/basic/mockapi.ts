92344c7253d4481898a98d3865d319ebb24cbe31
tadelesh <tadelesh.shi@live.cn>
2024-04-25 17:28:13 +0800

    fix

--- packages/cadl-ranch-specs/http/parameters/basic/mockapi.ts
+++ packages/cadl-ranch-specs/http/parameters/basic/mockapi.ts
@@ -3,14 +3,14 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Parameters_Basic_Explicit_simple = passOnSuccess(
+Scenarios.Parameters_Basic_ExplicitBody_simple = passOnSuccess(
   mockapi.put("/parameters/basic/explicit-body/simple", (req) => {
     req.expect.bodyEquals({ name: "foo" });
     return { status: 204 };
   }),
 );
 
-Scenarios.Parameters_Basic_Implicit_simple = passOnSuccess(
+Scenarios.Parameters_Basic_ImplicitBody_simple = passOnSuccess(
   mockapi.put("/parameters/basic/implicit-body/simple", (req) => {
     req.expect.bodyEquals({ name: "foo" });
     return { status: 204 };
