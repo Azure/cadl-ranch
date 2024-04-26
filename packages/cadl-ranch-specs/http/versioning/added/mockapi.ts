dbef2d09fcca6930244441c403b6aca2f3d0679b
tadelesh <tadelesh.shi@live.cn>
2024-04-26 17:25:53 +0800

    add test for versioning

---
+++ packages/cadl-ranch-specs/http/versioning/added/mockapi.ts
@@ -0,0 +1,35 @@
+import { passOnSuccess, mockapi, json } from "@azure-tools/cadl-ranch-api";
+import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
+
+export const Scenarios: Record<string, ScenarioMockApi> = {};
+
+Scenarios.Versioning_Added_v1 = passOnSuccess(
+  mockapi.post("/versioning/added/api-version:v2/v1", (req) => {
+    req.expect.bodyEquals({ prop: "foo", enumProp: "enumMemberV2", unionProp: 10 });
+    req.expect.containsHeader("header-v2", "bar");
+    return {
+      status: 200,
+      body: json({ prop: "foo", enumProp: "enumMemberV2", unionProp: 10 }),
+    };
+  }),
+);
+
+Scenarios.Versioning_Added_v2 = passOnSuccess(
+  mockapi.post("/versioning/added/api-version:v2/v2", (req) => {
+    req.expect.bodyEquals({ prop: "foo", enumProp: "enumMember", unionProp: "bar" });
+    return {
+      status: 200,
+      body: json({ prop: "foo", enumProp: "enumMember", unionProp: "bar" }),
+    };
+  }),
+);
+
+Scenarios.Versioning_Added_InterfaceV2_v2InInterface = passOnSuccess(
+  mockapi.post("/versioning/added/api-version:v2/interface-v2/v2", (req) => {
+    req.expect.bodyEquals({ prop: "foo", enumProp: "enumMember", unionProp: "bar" });
+    return {
+      status: 200,
+      body: json({ prop: "foo", enumProp: "enumMember", unionProp: "bar" }),
+    };
+  }),
+);
