aa92a62785a79e4ee2b9a6d35476612a39f2b9e5
xiaofeicao <xiaofeicao@microsoft.com>
2024-05-27 23:21:45 +0800

    nested

--- packages/cadl-ranch-specs/http/arm/models/resources/mockapi.ts
+++ packages/cadl-ranch-specs/http/arm/models/resources/mockapi.ts
@@ -1,15 +1,15 @@
-import { passOnSuccess, mockapi, json, MockApi } from "@azure-tools/cadl-ranch-api";
+import { passOnSuccess, mockapi, json } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-const resource = {
+const dummyTopLevelResource = {
   id: "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Arm.Models.Resources/topLevelArmResources/topLevelResource",
   name: "topLevelResource",
-  type: "dummy",
+  type: "topLevel",
   properties: {
-    "provisioningState": "Succeeded",
-    "description": "any string"
+    provisioningState: "Succeeded",
+    description: "any string",
   },
   systemData: {
     createdBy: "AzureSDK",
@@ -18,93 +18,306 @@ const resource = {
     lastModifiedBy: "AzureSDK",
     lastModifiedAt: new Date(),
     lastModifiedByType: "User",
-  }
-}
+  },
+};
 
 Scenarios.Arm_Models_Resources_TopLevelArmResources_get = passOnSuccess([
-  mockapi.get("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Arm.Models.Resources/topLevelArmResources/topLevelResource", (req) => {
-    req.expect.containsQueryParam("api-version", "2023-12-01-preview");
-    return {
-      status: 200,
-      body: json(resource)
-    }
-  })
+  mockapi.get(
+    "/subscriptions/:subscriptionId/resourceGroups/:resourceGroup/providers/Arm.Models.Resources/topLevelArmResources/:topLevelResourceName",
+    (req) => {
+      req.expect.containsQueryParam("api-version", "2023-12-01-preview");
+      return {
+        status: 200,
+        body: json({
+          id: req.originalRequest.path,
+          name: req.params.topLevelResourceName,
+          type: "topLevel",
+          properties: {
+            provisioningState: "Succeeded",
+            description: "any string",
+          },
+          systemData: {
+            createdBy: "AzureSDK",
+            createdByType: "User",
+            createdAt: new Date(),
+            lastModifiedBy: "AzureSDK",
+            lastModifiedAt: new Date(),
+            lastModifiedByType: "User",
+          },
+        }),
+      };
+    },
+  ),
 ]);
 
 Scenarios.Arm_Models_Resources_TopLevelArmResources_createOrReplace = passOnSuccess([
-  mockapi.put("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Arm.Models.Resources/topLevelArmResources/topLevelResource", (req) => {
-    req.expect.containsQueryParam("api-version", "2023-12-01-preview");
-    req.expect.bodyNotEmpty();
-    const body = req.body;
-    const description = body.properties?.description;
-    if (!Boolean(description)) {
-      return {
-        status: "400"
+  mockapi.put(
+    "/subscriptions/:subscriptionId/resourceGroups/:resourceGroup/providers/Arm.Models.Resources/topLevelArmResources/:topLevelResourceName",
+    (req) => {
+      req.expect.containsQueryParam("api-version", "2023-12-01-preview");
+      req.expect.bodyNotEmpty();
+      const body = req.body;
+      const description = body.properties?.description;
+      if (!Boolean(description)) {
+        return {
+          status: 400,
+        };
       }
-    }
-    return {
-      status: 200,
-      body: json(resource)
-    }
-  })
-]);
-
-function createGetApi(url: string): MockApi {
-  return mockapi.get(url, (req) => {
-    req.expect.containsQueryParam("api-version", "2023-12-01-preview");
-    const topLevelArmResource = repository.get(req.originalRequest.path);
-    if (!Boolean(topLevelArmResource)) {
-      return {
-        status: 404,
-      };
-    } else {
       return {
         status: 200,
-        body: json(topLevelArmResource),
+        body: json({
+          id: req.originalRequest.path,
+          name: req.params.topLevelResourceName,
+          type: "topLevel",
+          properties: {
+            provisioningState: "Succeeded",
+            description: description,
+          },
+          systemData: {
+            createdBy: "AzureSDK",
+            createdByType: "User",
+            createdAt: new Date(),
+            lastModifiedBy: "AzureSDK",
+            lastModifiedAt: new Date(),
+            lastModifiedByType: "User",
+          },
+        }),
       };
-    }
-  });
-}
+    },
+  ),
+]);
 
-function createPutApi(url: string): MockApi {
-  return mockapi.put(url, (req) => {
+Scenarios.Arm_Models_Resources_TopLevelArmResources_update = passOnSuccess([
+  mockapi.put(
+    "/subscriptions/:subscriptionId/resourceGroups/:resourceGroup/providers/Arm.Models.Resources/topLevelArmResources/:topLevelResourceName",
+    (req) => {
+      req.expect.containsQueryParam("api-version", "2023-12-01-preview");
+      req.expect.bodyNotEmpty();
+      const body = req.body;
+      const description = body.properties?.description;
+      if (!Boolean(description)) {
+        return {
+          status: 400,
+        };
+      }
+      return {
+        status: 200,
+        body: json({
+          id: req.originalRequest.path,
+          name: req.params.topLevelResourceName,
+          type: "topLevel",
+          properties: {
+            provisioningState: "Succeeded",
+            description: description,
+          },
+          systemData: {
+            createdBy: "AzureSDK",
+            createdByType: "User",
+            createdAt: new Date(),
+            lastModifiedBy: "AzureSDK",
+            lastModifiedAt: new Date(),
+            lastModifiedByType: "User",
+          },
+        }),
+      };
+    },
+  ),
+]);
+
+Scenarios.Arm_Models_Resources_TopLevelArmResources_delete = passOnSuccess([
+  mockapi.delete(
+    "/subscriptions/:subscriptionId/resourceGroups/:resourceGroup/providers/Arm.Models.Resources/topLevelArmResources/:topLevelResourceName",
+    (req) => {
+      req.expect.containsQueryParam("api-version", "2023-12-01-preview");
+      return {
+        status: 204,
+      };
+    },
+  ),
+]);
+
+Scenarios.Arm_Models_Resources_TopLevelArmResources_listByResourceGroup = passOnSuccess([
+  mockapi.get(
+    "/subscriptions/:subscriptionId/resourceGroups/:resourceGroup/providers/Arm.Models.Resources/topLevelArmResources",
+    (req) => {
+      req.expect.containsQueryParam("api-version", "2023-12-01-preview");
+      const resource = {
+        ...dummyTopLevelResource,
+      };
+      resource.id = resource.id
+        .replace("00000000-0000-0000-0000-000000000000", req.params.subscriptionId)
+        .replace("/test-rg/", `/${req.params.resourceGroup}/`);
+      return {
+        status: 200,
+        body: json({
+          value: [resource],
+        }),
+      };
+    },
+  ),
+]);
+
+Scenarios.Arm_Models_Resources_TopLevelArmResources_listBySubscription = passOnSuccess([
+  mockapi.get("/subscriptions/:subscriptionId/providers/Arm.Models.Resources/topLevelArmResources", (req) => {
     req.expect.containsQueryParam("api-version", "2023-12-01-preview");
-    req.expect.bodyNotEmpty();
+    const resource = {
+      ...dummyTopLevelResource,
+    };
+    resource.id = resource.id.replace("00000000-0000-0000-0000-000000000000", req.params.subscriptionId);
     return {
       status: 200,
-      body: json(repository.put(req.originalRequest.path, req.params.topLevelResourceName, req.body)),
+      body: json({
+        value: [resource],
+      }),
     };
-  });
-}
+  }),
+]);
 
-function createListBySubscriptionApi(url: string): MockApi {
-  return mockapi.get(url, (req) => {
-    req.expect.containsQueryParam("api-version", "2023-12-01-preview");
-    const resources = repository.listBySubscription(req.originalRequest.path)
-    return {
-      status: 200,
-      body: json(resources),
-    };
-  });
-}
+// nested child resource
+Scenarios.Arm_Models_Resources_NestedChildResources_get = passOnSuccess([
+  mockapi.get(
+    "/subscriptions/:subscriptionId/resourceGroups/:resourceGroup/providers/Arm.Models.Resources/topLevelArmResources/:topLevelResourceName/nestedResources/:nestedResourceName",
+    (req) => {
+      req.expect.containsQueryParam("api-version", "2023-12-01-preview");
+      return {
+        status: 200,
+        body: json({
+          id: req.originalRequest.path,
+          name: req.params.nestedResourceName,
+          type: "nested",
+          properties: {
+            provisioningState: "Succeeded",
+            description: "any string",
+          },
+          systemData: {
+            createdBy: "AzureSDK",
+            createdByType: "User",
+            createdAt: new Date(),
+            lastModifiedBy: "AzureSDK",
+            lastModifiedAt: new Date(),
+            lastModifiedByType: "User",
+          },
+        }),
+      };
+    },
+  ),
+]);
 
-function createListByResourceGroupApi(url: string): MockApi {
-  return mockapi.get(url, (req) => {
-    req.expect.containsQueryParam("api-version", "2023-12-01-preview");
-    const resources = repository.listByResourceGroup(req.originalRequest.path)
-    return {
-      status: 200,
-      body: json(resources),
-    };
-  });
-}
+Scenarios.Arm_Models_Resources_NestedChildResources_createOrReplace = passOnSuccess([
+  mockapi.put(
+    "/subscriptions/:subscriptionId/resourceGroups/:resourceGroup/providers/Arm.Models.Resources/topLevelArmResources/:topLevelResourceName/nestedResources/:nestedResourceName",
+    (req) => {
+      req.expect.containsQueryParam("api-version", "2023-12-01-preview");
+      req.expect.bodyNotEmpty();
+      const body = req.body;
+      const description = body.properties?.description;
+      if (!Boolean(description)) {
+        return {
+          status: 400,
+        };
+      }
+      return {
+        status: 200,
+        body: json({
+          id: req.originalRequest.path,
+          name: req.params.nestedResourceName,
+          type: "nested",
+          properties: {
+            provisioningState: "Succeeded",
+            description: description,
+          },
+          systemData: {
+            createdBy: "AzureSDK",
+            createdByType: "User",
+            createdAt: new Date(),
+            lastModifiedBy: "AzureSDK",
+            lastModifiedAt: new Date(),
+            lastModifiedByType: "User",
+          },
+        }),
+      };
+    },
+  ),
+]);
 
-function createDeleteApi(url: string): MockApi {
-  return mockapi.delete(url, (req) => {
-    req.expect.containsQueryParam("api-version", "2023-12-01-preview");
-    repository.delete(req.originalRequest.path);
-    return {
-      status: 204
-    }
-  })
-}
+Scenarios.Arm_Models_Resources_NestedChildResources_update = passOnSuccess([
+  mockapi.put(
+    "/subscriptions/:subscriptionId/resourceGroups/:resourceGroup/providers/Arm.Models.Resources/topLevelArmResources/:topLevelResourceName/nestedResources/:nestedResourceName",
+    (req) => {
+      req.expect.containsQueryParam("api-version", "2023-12-01-preview");
+      req.expect.bodyNotEmpty();
+      const body = req.body;
+      const description = body.properties?.description;
+      if (!Boolean(description)) {
+        return {
+          status: 400,
+        };
+      }
+      return {
+        status: 200,
+        body: json({
+          id: req.originalRequest.path,
+          name: req.params.nestedResourceName,
+          type: "nested",
+          properties: {
+            provisioningState: "Succeeded",
+            description: description,
+          },
+          systemData: {
+            createdBy: "AzureSDK",
+            createdByType: "User",
+            createdAt: new Date(),
+            lastModifiedBy: "AzureSDK",
+            lastModifiedAt: new Date(),
+            lastModifiedByType: "User",
+          },
+        }),
+      };
+    },
+  ),
+]);
+
+Scenarios.Arm_Models_Resources_NestedChildResources_delete = passOnSuccess([
+  mockapi.delete(
+    "/subscriptions/:subscriptionId/resourceGroups/:resourceGroup/providers/Arm.Models.Resources/topLevelArmResources/:topLevelResourceName/nestedResources/:nestedResourceName",
+    (req) => {
+      req.expect.containsQueryParam("api-version", "2023-12-01-preview");
+      return {
+        status: 204,
+      };
+    },
+  ),
+]);
+
+Scenarios.Arm_Models_Resources_NestedChildResources_listByTopLevelArmResource = passOnSuccess([
+  mockapi.get(
+    "/subscriptions/:subscriptionId/resourceGroups/:resourceGroup/providers/Arm.Models.Resources/topLevelArmResources/:topLevelResourceName",
+    (req) => {
+      req.expect.containsQueryParam("api-version", "2023-12-01-preview");
+      return {
+        status: 200,
+        body: json({
+          value: [
+            {
+              id: `/subscriptions/${req.params.subscriptionId}/resourceGroups/${req.params.resourceGroup}/providers/Arm.Models.Resources/topLevelArmResources/${req.params.topLevelResourceName}/nestedResources/nestedResource`,
+              name: req.params.nestedResourceName,
+              type: "nested",
+              properties: {
+                provisioningState: "Succeeded",
+                description: "any string",
+              },
+              systemData: {
+                createdBy: "AzureSDK",
+                createdByType: "User",
+                createdAt: new Date(),
+                lastModifiedBy: "AzureSDK",
+                lastModifiedAt: new Date(),
+                lastModifiedByType: "User",
+              },
+            },
+          ],
+        }),
+      };
+    },
+  ),
+]);
