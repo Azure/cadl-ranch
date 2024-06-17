45b7c11c66f5647ed87b7e5f4045d7f081df44a0
mcgallan <1186003288@qq.com>
2024-06-17 10:23:18 +0800

    update

--- packages/cadl-ranch-specs/http/versioning/anomaly/mockapi.ts
+++ packages/cadl-ranch-specs/http/versioning/anomaly/mockapi.ts
@@ -3,8 +3,8 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 const detectionResult = { resultId: "73f411fe-4f43-4b4b-9cbd-6828d8f4cf9a" };
-Scenarios.Versioning_AnomalyDetector_GetAnomalyDetectionResult = passOnSuccess(
-  mockapi.get("/versioning/anomalydetector/v1/result/73f411fe-4f43-4b4b-9cbd-6828d8f4cf9a", (req) => {
+Scenarios.Versioning_Anomaly_GetAnomalyDetectionResult = passOnSuccess(
+  mockapi.get("/versioning/anomaly/v1/result/73f411fe-4f43-4b4b-9cbd-6828d8f4cf9a", (req) => {
     return {
       status: 200,
       body: json(detectionResult),
