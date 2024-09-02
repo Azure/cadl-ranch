a721e9de08fca2e19ee41949fedacf84726f3ac0
mcgallan <1186003288@qq.com>
2024-09-02 11:32:36 +0800

    Update mockapi.ts

--- packages/cadl-ranch-specs/http/versioning/removed/mockapi.ts
+++ packages/cadl-ranch-specs/http/versioning/removed/mockapi.ts
@@ -1,7 +1,39 @@
-import { passOnSuccess, mockapi, json } from "@azure-tools/cadl-ranch-api";
+import { mockapi, MockApi, passOnSuccess, json } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
+
+export const commonBase = "/resiliency/service-driven";
+
+type PassResiliencyOptions = {
+  version: string;
+};
+
+function createResilientMockApi(options: PassResiliencyOptions): MockApi[] {
+  return [
+    mockapi.post(`/versioning/removed/api-version:${options.version}/v3`, (req) => {
+      req.expect.bodyEquals({ id: "123", enumProp: "enumMemberV1" });
+      return {
+        status: 204,
+        body: json({ id: "123", enumProp: "enumMemberV1" }),
+      };
+    }),
+    mockapi.post("/versioning/removed/api-version:beta/v3", (req) => {
+      req.expect.bodyEquals({ id: "123", enumProp: "enumMemberBeta" });
+      return {
+        status: 204,
+        body: json({ id: "123", enumProp: "enumMemberBeta" }),
+      };
+    }),
+    mockapi.post("/versioning/removed/api-version:v2/v3", (req) => {
+      req.expect.bodyEquals({ id: "123", enumProp: "enumMemberV1" });
+      return {
+        status: 204,
+        body: json({ id: "123", enumProp: "enumMemberV1" }),
+      };
+    }),
+  ];
+}
 
 Scenarios.Versioning_Removed_v2 = passOnSuccess(
   mockapi.post("/versioning/removed/api-version:v2/v2", (req) => {
@@ -13,32 +45,20 @@ Scenarios.Versioning_Removed_v2 = passOnSuccess(
   }),
 );
 
-Scenarios.Versioning_Removed_modelV3 = passOnSuccess(
-  mockapi.post("/versioning/removed/api-version:v1/v3", (req) => {
-    req.expect.bodyEquals({ id: "123", enumProp: "enumMemberV1" });
-    return {
-      status: 200,
-      body: json({ id: "123", enumProp: "enumMemberV1" }),
-    };
+Scenarios.Versioning_Removed_modelV3_version1 = passOnSuccess(
+  createResilientMockApi({
+    version: "v1",
   }),
 );
 
-Scenarios.Versioning_Removed_modelV3_beta = passOnSuccess(
-  mockapi.post("/versioning/removed/api-version:beta/v3", (req) => {
-    req.expect.bodyEquals({ id: "123", enumProp: "enumMemberBeta" });
-    return {
-      status: 200,
-      body: json({ id: "123", enumProp: "enumMemberBeta" }),
-    };
+Scenarios.Versioning_Removed_modelV3_versionbeta = passOnSuccess(
+  createResilientMockApi({
+    version: "beta",
   }),
 );
 
-Scenarios.Versioning_Removed_modelV3_v2 = passOnSuccess(
-  mockapi.post("/versioning/removed/api-version:v2/v3", (req) => {
-    req.expect.bodyEquals({ id: "123", enumProp: "enumMemberV1" });
-    return {
-      status: 200,
-      body: json({ id: "123", enumProp: "enumMemberV1" }),
-    };
+Scenarios.Versioning_Removed_modelV3_version2 = passOnSuccess(
+  createResilientMockApi({
+    version: "v2",
   }),
 );
