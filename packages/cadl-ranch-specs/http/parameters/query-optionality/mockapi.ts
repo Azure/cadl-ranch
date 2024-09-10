3712d7c55ded5c21e5fd0c549d07addca4b6bf4d
mcgallan <1186003288@qq.com>
2024-09-10 10:31:23 +0800

    Update mockapi.ts

--- packages/cadl-ranch-specs/http/parameters/query-optionality/mockapi.ts
+++ packages/cadl-ranch-specs/http/parameters/query-optionality/mockapi.ts
@@ -3,7 +3,7 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Parameters_QueryOptionality_fromrequired = passOnSuccess(
+Scenarios.Parameters_QueryOptionality_FromRequired = passOnSuccess(
   mockapi.head("/parameters/query-optionality/fromrequired", (req) => {
     req.expect.containsQueryParam("api-version", "2022-12-01-preview");
     req.expect.containsQueryParam("start", "required");
@@ -11,7 +11,7 @@ Scenarios.Parameters_QueryOptionality_fromrequired = passOnSuccess(
   }),
 );
 
-Scenarios.Parameters_QueryOptionality_fromoptional = passOnSuccess(
+Scenarios.Parameters_QueryOptionality_FromOptional = passOnSuccess(
   mockapi.head("/parameters/query-optionality/fromoptional", (req) => {
     req.expect.containsQueryParam("end", "required");
     return { status: 204 };
