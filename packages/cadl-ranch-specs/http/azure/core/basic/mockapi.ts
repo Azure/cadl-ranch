Azure/cadl-ranch

commit 352934c79dcc932e8c7ca2ecd6a7728447aa3747

[cadl-ranch-*] Backport the server testing changes from typespec repository to cadl-ranch repository (#741)
* Initial Commit

* Format and Changeset addition

packages/cadl-ranch-specs/http/azure/core/basic/mockapi.ts
@@ -1,11 +1,24 @@
-import { passOnSuccess, mockapi, json, ValidationError } from "@azure-tools/cadl-ranch-api";
+import { passOnSuccess, json, ValidationError, MockRequest } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 const validUser = { id: 1, name: "Madge", etag: "11bdc430-65e8-45ad-81d9-8ffa60d55b59" };
 const validUser2 = { id: 2, name: "John", etag: "22bdc430-65e8-45ad-81d9-8ffa60d55b59" };
-Scenarios.Azure_Core_Basic_createOrUpdate = passOnSuccess(
-  mockapi.patch("/azure/core/basic/users/:id", (req) => {
+Scenarios.Azure_Core_Basic_createOrUpdate = passOnSuccess({
+  uri: "/azure/core/basic/users/:id",
+  method: "patch",
+  request: {
+    params: {
+      "id": "1",
+      "api-version": "2022-12-01-preview",
+    },
+    headers: {
+      "Content-Type": "application/merge-patch+json",
+    },
+    body: { name: "Madge" },
+  },
+  response: { status: 200, body: json(validUser) },
+  handler: (req: MockRequest) => {
     if (req.params.id !== "1") {
       throw new ValidationError("Expected path param id=1", "1", req.params.id);
     }
@@ -14,11 +27,22 @@ Scenarios.Azure_Core_Basic_createOrUpdate = passOnSuccess(
     const validBody = { name: "Madge" };
     req.expect.bodyEquals(validBody);
     return { status: 200, body: json(validUser) };
-  }),
-);
+  },
+  kind: "MockApiDefinition",
+});
 
-Scenarios.Azure_Core_Basic_createOrReplace = passOnSuccess(
-  mockapi.put("/azure/core/basic/users/:id", (req) => {
+Scenarios.Azure_Core_Basic_createOrReplace = passOnSuccess({
+  uri: "/azure/core/basic/users/:id",
+  method: "put",
+  request: {
+    params: {
+      "id": "1",
+      "api-version": "2022-12-01-preview",
+    },
+    body: { name: "Madge" },
+  },
+  response: { status: 200, body: json(validUser) },
+  handler: (req: MockRequest) => {
     if (req.params.id !== "1") {
       throw new ValidationError("Expected path param id=1", "1", req.params.id);
     }
@@ -27,80 +51,141 @@ Scenarios.Azure_Core_Basic_createOrReplace = passOnSuccess(
     const validBody = { name: "Madge" };
     req.expect.bodyEquals(validBody);
     return { status: 200, body: json(validUser) };
-  }),
-);
+  },
+  kind: "MockApiDefinition",
+});
 
-Scenarios.Azure_Core_Basic_get = passOnSuccess(
-  mockapi.get("/azure/core/basic/users/:id", (req) => {
+Scenarios.Azure_Core_Basic_get = passOnSuccess({
+  uri: "/azure/core/basic/users/:id",
+  method: "get",
+  request: {
+    params: {
+      "id": "1",
+      "api-version": "2022-12-01-preview",
+    },
+  },
+  response: { status: 200, body: json(validUser) },
+  handler: (req: MockRequest) => {
     if (req.params.id !== "1") {
       throw new ValidationError("Expected path param id=1", "1", req.params.id);
     }
     req.expect.containsQueryParam("api-version", "2022-12-01-preview");
     return { status: 200, body: json(validUser) };
-  }),
-);
-
-Scenarios.Azure_Core_Basic_list = passOnSuccess(
-  mockapi.get("/azure/core/basic/users", (req) => {
+  },
+  kind: "MockApiDefinition",
+});
+const responseBody = {
+  value: [
+    {
+      id: 1,
+      name: "Madge",
+      etag: "11bdc430-65e8-45ad-81d9-8ffa60d55b59",
+      orders: [{ id: 1, userId: 1, detail: "a recorder" }],
+    },
+    {
+      id: 2,
+      name: "John",
+      etag: "11bdc430-65e8-45ad-81d9-8ffa60d55b5a",
+      orders: [{ id: 2, userId: 2, detail: "a TV" }],
+    },
+  ],
+};
+Scenarios.Azure_Core_Basic_list = passOnSuccess({
+  uri: "/azure/core/basic/users",
+  method: "get",
+  request: {
+    params: {
+      "api-version": "2022-12-01-preview",
+      "top": 5,
+      "skip": 10,
+      "orderby": "id",
+      "filter": "id lt 10",
+      "select": ["id", "orders", "etag"],
+      "expand": "orders",
+    },
+  },
+  response: { status: 200, body: json(responseBody) },
+  handler: (req: MockRequest) => {
     req.expect.containsQueryParam("api-version", "2022-12-01-preview");
     req.expect.containsQueryParam("top", "5");
     req.expect.containsQueryParam("skip", "10");
     req.expect.containsQueryParam("orderby", "id");
     req.expect.containsQueryParam("filter", "id lt 10");
-    if (!req.originalRequest.originalUrl.includes("select=id&select=orders&select=etag")) {
+    if (!req.originalRequest.originalUrl.includes("select[]=id&select[]=orders&select[]=etag")) {
       throw new ValidationError(
-        "Expected query param select=id&select=orders&select=etag ",
-        "select=id&select=orders&select=etag",
+        "Expected query param select[]=id&select[]=orders&select[]=etag ",
+        "select[]=id&select[]=orders&select[]=etag",
         req.originalRequest.originalUrl,
       );
     }
     req.expect.containsQueryParam("expand", "orders");
-    const responseBody = {
-      value: [
-        {
-          id: 1,
-          name: "Madge",
-          etag: "11bdc430-65e8-45ad-81d9-8ffa60d55b59",
-          orders: [{ id: 1, userId: 1, detail: "a recorder" }],
-        },
-        {
-          id: 2,
-          name: "John",
-          etag: "11bdc430-65e8-45ad-81d9-8ffa60d55b5a",
-          orders: [{ id: 2, userId: 2, detail: "a TV" }],
-        },
-      ],
-    };
     return { status: 200, body: json(responseBody) };
-  }),
-);
+  },
+  kind: "MockApiDefinition",
+});
 
-Scenarios.Azure_Core_Basic_delete = passOnSuccess(
-  mockapi.delete("/azure/core/basic/users/:id", (req) => {
+Scenarios.Azure_Core_Basic_delete = passOnSuccess({
+  uri: "/azure/core/basic/users/:id",
+  method: "delete",
+  request: {
+    params: {
+      "id": "1",
+      "api-version": "2022-12-01-preview",
+    },
+  },
+  response: {
+    status: 204,
+  },
+  handler: (req: MockRequest) => {
     if (req.params.id !== "1") {
       throw new ValidationError("Expected path param id=1", "1", req.params.id);
     }
     req.expect.containsQueryParam("api-version", "2022-12-01-preview");
     return { status: 204 };
-  }),
-);
+  },
+  kind: "MockApiDefinition",
+});
 
-Scenarios.Azure_Core_Basic_export = passOnSuccess(
-  mockapi.post("/azure/core/basic/users/:id[:]export", (req) => {
+Scenarios.Azure_Core_Basic_export = passOnSuccess({
+  uri: "/azure/core/basic/users/:id[:]export",
+  method: "post",
+  request: {
+    params: {
+      "id": "1",
+      "format": "json",
+      "api-version": "2022-12-01-preview",
+    },
+  },
+  response: {
+    status: 200,
+    body: json(validUser),
+  },
+  handler: (req: MockRequest) => {
     if (req.params.id !== "1") {
       throw new ValidationError("Expected path param id=1", "1", req.params.id);
     }
     req.expect.containsQueryParam("api-version", "2022-12-01-preview");
     req.expect.containsQueryParam("format", "json");
     return { status: 200, body: json(validUser) };
-  }),
-);
+  },
+  kind: "MockApiDefinition",
+});
 
 const expectBody = { users: [validUser, validUser2] };
-Scenarios.Azure_Core_Basic_exportAllUsers = passOnSuccess(
-  mockapi.post("/azure/core/basic/users:exportallusers", (req) => {
+Scenarios.Azure_Core_Basic_exportAllUsers = passOnSuccess({
+  uri: "/azure/core/basic/users:exportallusers",
+  method: "post",
+  request: {
+    params: {
+      "format": "json",
+      "api-version": "2022-12-01-preview",
+    },
+  },
+  response: { status: 200, body: json(expectBody) },
+  handler: (req: MockRequest) => {
     req.expect.containsQueryParam("api-version", "2022-12-01-preview");
     req.expect.containsQueryParam("format", "json");
     return { status: 200, body: json(expectBody) };
-  }),
-);
+  },
+  kind: "MockApiDefinition",
+});

packages/cadl-ranch-specs/http/encode/bytes/mockapi.ts
@@ -1,4 +1,4 @@
-import { passOnSuccess, mockapi, json, CollectionFormat, MockApi } from "@azure-tools/cadl-ranch-api";
+import { passOnSuccess, json, CollectionFormat, MockRequest } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 import { resolvePath } from "@typespec/compiler";
 import { readFileSync } from "fs";
@@ -10,98 +10,291 @@ const pngFile = readFileSync(resolvePath(root, "assets/image.png"));
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-function createQueryMockApis(route: string, value: any, collectionFormat?: CollectionFormat): MockApi {
-  const url = `/encode/bytes/query/${route}`;
-  return mockapi.get(url, (req) => {
-    req.expect.containsQueryParam("value", value, collectionFormat);
-    return {
+function createQueryServerTests(uri: string, data: any, value: any, collectionFormat?: CollectionFormat) {
+  return passOnSuccess({
+    uri,
+    method: "get",
+    request: {
+      params: data,
+    },
+    response: {
       status: 204,
-    };
+    },
+    handler: (req: MockRequest) => {
+      req.expect.containsQueryParam("value", value, collectionFormat);
+      return {
+        status: 204,
+      };
+    },
+    kind: "MockApiDefinition",
   });
 }
-
-function createPropertyMockApis(route: string, value: any): MockApi {
-  const url = `/encode/bytes/property/${route}`;
-  return mockapi.post(url, (req) => {
-    req.expect.coercedBodyEquals({ value: value });
-    return {
+Scenarios.Encode_Bytes_Query_default = createQueryServerTests(
+  "/encode/bytes/query/default",
+  {
+    value: "dGVzdA==",
+  },
+  "dGVzdA==",
+);
+Scenarios.Encode_Bytes_Query_base64 = createQueryServerTests(
+  "/encode/bytes/query/base64",
+  {
+    value: "dGVzdA==",
+  },
+  "dGVzdA==",
+);
+Scenarios.Encode_Bytes_Query_base64url = createQueryServerTests(
+  "/encode/bytes/query/base64url",
+  {
+    value: "dGVzdA",
+  },
+  "dGVzdA",
+);
+Scenarios.Encode_Bytes_Query_base64urlArray = createQueryServerTests(
+  "/encode/bytes/query/base64url-array",
+  {
+    value: ["dGVzdA", "dGVzdA"].join(","),
+  },
+  ["dGVzdA", "dGVzdA"],
+  "csv",
+);
+function createPropertyServerTests(uri: string, data: any, value: any) {
+  return passOnSuccess({
+    uri,
+    method: "post",
+    request: {
+      body: data,
+    },
+    response: {
       status: 200,
-      body: json({ value: value }),
-    };
+    },
+    handler: (req: MockRequest) => {
+      req.expect.coercedBodyEquals({ value: value });
+      return {
+        status: 200,
+        body: json({ value: value }),
+      };
+    },
+    kind: "MockApiDefinition",
   });
 }
-
-function createHeaderMockApis(route: string, value: any): MockApi {
-  const url = `/encode/bytes/header/${route}`;
-  return mockapi.get(url, (req) => {
-    req.expect.containsHeader("value", value);
-    return {
+Scenarios.Encode_Bytes_Property_default = createPropertyServerTests(
+  "/encode/bytes/property/default",
+  {
+    value: "dGVzdA==",
+  },
+  "dGVzdA==",
+);
+Scenarios.Encode_Bytes_Property_base64 = createPropertyServerTests(
+  "/encode/bytes/property/base64",
+  {
+    value: "dGVzdA==",
+  },
+  "dGVzdA==",
+);
+Scenarios.Encode_Bytes_Property_base64url = createPropertyServerTests(
+  "/encode/bytes/property/base64url",
+  {
+    value: "dGVzdA",
+  },
+  "dGVzdA",
+);
+Scenarios.Encode_Bytes_Property_base64urlArray = createPropertyServerTests(
+  "/encode/bytes/property/base64url-array",
+  {
+    value: ["dGVzdA", "dGVzdA"],
+  },
+  ["dGVzdA", "dGVzdA"],
+);
+function createHeaderServerTests(uri: string, data: any, value: any) {
+  return passOnSuccess({
+    uri,
+    method: "get",
+    request: {
+      headers: data,
+    },
+    response: {
       status: 204,
-    };
+    },
+    handler: (req: MockRequest) => {
+      req.expect.containsHeader("value", value);
+      return {
+        status: 204,
+      };
+    },
+    kind: "MockApiDefinition",
   });
 }
-
-function createRequestBodyMockApis(route: string, value: any, contentType: string = "application/json"): MockApi {
-  const url = `/encode/bytes/body/request/${route}`;
-  return mockapi.post(url, (req) => {
-    req.expect.containsHeader("content-type", contentType);
-    req.expect.rawBodyEquals(value);
-    return {
+Scenarios.Encode_Bytes_Header_default = createHeaderServerTests(
+  "/encode/bytes/header/default",
+  {
+    value: "dGVzdA==",
+  },
+  "dGVzdA==",
+);
+Scenarios.Encode_Bytes_Header_base64 = createHeaderServerTests(
+  "/encode/bytes/header/base64",
+  {
+    value: "dGVzdA==",
+  },
+  "dGVzdA==",
+);
+Scenarios.Encode_Bytes_Header_base64url = createHeaderServerTests(
+  "/encode/bytes/header/base64url",
+  {
+    value: "dGVzdA",
+  },
+  "dGVzdA",
+);
+Scenarios.Encode_Bytes_Header_base64urlArray = createHeaderServerTests(
+  "/encode/bytes/header/base64url-array",
+  {
+    value: ["dGVzdA", "dGVzdA"].join(","),
+  },
+  ["dGVzdA", "dGVzdA"].join(","),
+);
+function createRequestBodyServerTests(
+  uri: string,
+  data: any,
+  headersData: any,
+  value: any,
+  contentType: string = "application/json",
+) {
+  return passOnSuccess({
+    uri,
+    method: "post",
+    request: {
+      body: data,
+      headers: headersData,
+    },
+    response: {
       status: 204,
-    };
+    },
+    handler(req: MockRequest) {
+      req.expect.containsHeader("content-type", contentType);
+      req.expect.rawBodyEquals(value);
+      return {
+        status: 204,
+      };
+    },
+    kind: "MockApiDefinition",
   });
 }
-
-function createResponseBodyMockApis(route: string, value: any, contentType: string = "application/json"): MockApi {
-  const url = `/encode/bytes/body/response/${route}`;
-  return mockapi.get(url, (req) => {
-    return {
+// Request body
+Scenarios.Encode_Bytes_RequestBody_default = createRequestBodyServerTests(
+  "/encode/bytes/body/request/default",
+  '"dGVzdA=="',
+  {
+    "Content-Type": "application/json",
+  },
+  '"dGVzdA=="',
+);
+Scenarios.Encode_Bytes_RequestBody_octetStream = createRequestBodyServerTests(
+  "/encode/bytes/body/request/octet-stream",
+  pngFile,
+  {
+    "Content-Type": "application/octet-stream",
+  },
+  pngFile,
+  "application/octet-stream",
+);
+Scenarios.Encode_Bytes_RequestBody_customContentType = createRequestBodyServerTests(
+  "/encode/bytes/body/request/custom-content-type",
+  pngFile,
+  {
+    "Content-Type": "image/png",
+  },
+  pngFile,
+  "image/png",
+);
+Scenarios.Encode_Bytes_RequestBody_base64 = createRequestBodyServerTests(
+  "/encode/bytes/body/request/base64",
+  '"dGVzdA=="',
+  {
+    "Content-Type": "application/json",
+  },
+  '"dGVzdA=="',
+);
+Scenarios.Encode_Bytes_RequestBody_base64url = createRequestBodyServerTests(
+  "/encode/bytes/body/request/base64url",
+  '"dGVzdA=="',
+  {
+    "Content-Type": "application/json",
+  },
+  '"dGVzdA=="',
+);
+function createResponseBodyServerTests(
+  uri: string,
+  data: any,
+  headerData: any,
+  value: any,
+  contentType: string = "application/json",
+) {
+  return passOnSuccess({
+    uri,
+    method: "get",
+    request: {
+      headers: headerData,
+    },
+    response: {
       status: 200,
       body: {
         contentType: contentType,
-        rawContent: value,
+        rawContent: data,
       },
-    };
+    },
+    handler(req: MockRequest) {
+      return {
+        status: 200,
+        body: {
+          contentType: contentType,
+          rawContent: value,
+        },
+      };
+    },
+    kind: "MockApiDefinition",
   });
 }
-
-Scenarios.Encode_Bytes_Query_default = passOnSuccess(createQueryMockApis("default", "dGVzdA=="));
-Scenarios.Encode_Bytes_Query_base64 = passOnSuccess(createQueryMockApis("base64", "dGVzdA=="));
-Scenarios.Encode_Bytes_Query_base64url = passOnSuccess(createQueryMockApis("base64url", "dGVzdA"));
-Scenarios.Encode_Bytes_Query_base64urlArray = passOnSuccess(
-  createQueryMockApis("base64url-array", ["dGVzdA", "dGVzdA"], "csv"),
-);
-
-Scenarios.Encode_Bytes_Property_default = passOnSuccess(createPropertyMockApis("default", "dGVzdA=="));
-Scenarios.Encode_Bytes_Property_base64 = passOnSuccess(createPropertyMockApis("base64", "dGVzdA=="));
-Scenarios.Encode_Bytes_Property_base64url = passOnSuccess(createPropertyMockApis("base64url", "dGVzdA"));
-Scenarios.Encode_Bytes_Property_base64urlArray = passOnSuccess(
-  createPropertyMockApis("base64url-array", ["dGVzdA", "dGVzdA"]),
-);
-
-Scenarios.Encode_Bytes_Header_default = passOnSuccess(createHeaderMockApis("default", "dGVzdA=="));
-Scenarios.Encode_Bytes_Header_base64 = passOnSuccess(createHeaderMockApis("base64", "dGVzdA=="));
-Scenarios.Encode_Bytes_Header_base64url = passOnSuccess(createHeaderMockApis("base64url", "dGVzdA"));
-Scenarios.Encode_Bytes_Header_base64urlArray = passOnSuccess(createHeaderMockApis("base64url-array", "dGVzdA,dGVzdA"));
-
-// Request body
-Scenarios.Encode_Bytes_RequestBody_default = passOnSuccess(createRequestBodyMockApis("default", '"dGVzdA=="'));
-Scenarios.Encode_Bytes_RequestBody_octetStream = passOnSuccess(
-  createRequestBodyMockApis("octet-stream", pngFile, "application/octet-stream"),
-);
-Scenarios.Encode_Bytes_RequestBody_customContentType = passOnSuccess(
-  createRequestBodyMockApis("custom-content-type", pngFile, "image/png"),
-);
-Scenarios.Encode_Bytes_RequestBody_base64 = passOnSuccess(createRequestBodyMockApis("base64", '"dGVzdA=="'));
-Scenarios.Encode_Bytes_RequestBody_base64url = passOnSuccess(createRequestBodyMockApis("base64url", '"dGVzdA"'));
-
 // Response body
-Scenarios.Encode_Bytes_ResponseBody_default = passOnSuccess(createResponseBodyMockApis("default", '"dGVzdA=="'));
-Scenarios.Encode_Bytes_ResponseBody_octetStream = passOnSuccess(
-  createResponseBodyMockApis("octet-stream", pngFile, "application/octet-stream"),
+Scenarios.Encode_Bytes_ResponseBody_default = createResponseBodyServerTests(
+  "/encode/bytes/body/response/default",
+  JSON.stringify("dGVzdA=="),
+  {
+    "Content-Type": "application/json",
+  },
+  JSON.stringify("dGVzdA=="),
 );
-Scenarios.Encode_Bytes_ResponseBody_customContentType = passOnSuccess(
-  createResponseBodyMockApis("custom-content-type", pngFile, "image/png"),
+Scenarios.Encode_Bytes_ResponseBody_octetStream = createResponseBodyServerTests(
+  "/encode/bytes/body/response/octet-stream",
+  pngFile,
+  {
+    "Content-Type": "application/octet-stream",
+  },
+  pngFile,
+  "application/octet-stream",
 );
-Scenarios.Encode_Bytes_ResponseBody_base64 = passOnSuccess(createResponseBodyMockApis("base64", '"dGVzdA=="'));
-Scenarios.Encode_Bytes_ResponseBody_base64url = passOnSuccess(createResponseBodyMockApis("base64url", '"dGVzdA"'));
+Scenarios.Encode_Bytes_ResponseBody_customContentType = createResponseBodyServerTests(
+  "/encode/bytes/body/response/custom-content-type",
+  pngFile,
+  {
+    "Content-Type": "image/png",
+  },
+  pngFile,
+  "image/png",
+);
+Scenarios.Encode_Bytes_ResponseBody_base64 = createResponseBodyServerTests(
+  "/encode/bytes/body/response/base64",
+  JSON.stringify("dGVzdA=="),
+  {
+    "Content-Type": "application/json",
+  },
+  JSON.stringify("dGVzdA=="),
+);
+Scenarios.Encode_Bytes_ResponseBody_base64url = createResponseBodyServerTests(
+  "/encode/bytes/body/response/base64url",
+  JSON.stringify("dGVzdA"),
+  {
+    "Content-Type": "application/json",
+  },
+  JSON.stringify("dGVzdA"),
+);

packages/cadl-ranch-specs/http/parameters/body-optionality/mockapi.ts
@@ -1,29 +1,72 @@
-import { passOnSuccess, mockapi } from "@azure-tools/cadl-ranch-api";
+import { passOnSuccess, MockRequest } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
+function createServerTests(uri: string, data: any) {
+  return passOnSuccess({
+    uri,
+    method: "post",
+    request: {
+      body: data,
+    },
+    response: {
+      status: 204,
+    },
+    handler: (req: MockRequest) => {
+      req.expect.bodyEquals({ name: "foo" });
+      return { status: 204 };
+    },
+    kind: "MockApiDefinition",
+  });
+}
 
-Scenarios.Parameters_BodyOptionality_requiredExplicit = passOnSuccess(
-  mockapi.post("/parameters/body-optionality/required-explicit", (req) => {
-    req.expect.bodyEquals({ name: "foo" });
-    return { status: 204 };
-  }),
+Scenarios.Parameters_BodyOptionality_requiredExplicit = createServerTests(
+  "/parameters/body-optionality/required-explicit",
+  {
+    name: "foo",
+  },
 );
 
 Scenarios.Parameters_BodyOptionality_OptionalExplicit = passOnSuccess([
-  mockapi.post("/parameters/body-optionality/optional-explicit/set", (req) => {
-    req.expect.bodyEquals({ name: "foo" });
-    return { status: 204 };
-  }),
-  mockapi.post("/parameters/body-optionality/optional-explicit/omit", (req) => {
-    req.expect.rawBodyEquals(undefined);
-    return { status: 204 };
-  }),
+  {
+    uri: "/parameters/body-optionality/optional-explicit/set",
+    method: "post",
+    request: {
+      body: {
+        name: "foo",
+      },
+    },
+    response: {
+      status: 204,
+    },
+    handler: (req: MockRequest) => {
+      req.expect.bodyEquals({ name: "foo" });
+      return { status: 204 };
+    },
+    kind: "MockApiDefinition",
+  },
+  {
+    uri: "/parameters/body-optionality/optional-explicit/omit",
+    method: "post",
+    request: {
+      body: {
+        name: "foo",
+      },
+    },
+    response: {
+      status: 204,
+    },
+    handler: (req: MockRequest) => {
+      req.expect.bodyEquals({ name: "foo" });
+      return { status: 204 };
+    },
+    kind: "MockApiDefinition",
+  },
 ]);
 
-Scenarios.Parameters_BodyOptionality_requiredImplicit = passOnSuccess(
-  mockapi.post("/parameters/body-optionality/required-implicit", (req) => {
-    req.expect.bodyEquals({ name: "foo" });
-    return { status: 204 };
-  }),
+Scenarios.Parameters_BodyOptionality_requiredImplicit = createServerTests(
+  "/parameters/body-optionality/required-implicit",
+  {
+    name: "foo",
+  },
 );

packages/cadl-ranch-specs/http/authentication/api-key/mockapi.ts
@@ -1,17 +1,48 @@
-import { MockRequest } from "@azure-tools/cadl-ranch-api";
+import { json, MockRequest, passOnSuccess } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
-import { getValidAndInvalidScenarios } from "../commonapi.js";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-const validAndInvalidScenarios = getValidAndInvalidScenarios(
-  "api-key",
-  "invalid-api-key",
-  function addOptionalParamOldApiVersionNewClientValidate(req: MockRequest): void {
-    req.expect.containsHeader("x-ms-api-key", "valid-key");
+Scenarios.Authentication_ApiKey_valid = passOnSuccess({
+  uri: `/authentication/api-key/valid`,
+  method: `get`,
+  request: {
+    headers: {
+      "x-ms-api-key": "valid-key",
+    },
   },
-);
+  response: {
+    status: 204,
+  },
+  handler: (req: MockRequest) => {
+    req.expect.containsHeader("x-ms-api-key", "valid-key");
+    return { status: 204 };
+  },
+  kind: "MockApiDefinition",
+});
 
-Scenarios.Authentication_ApiKey_valid = validAndInvalidScenarios.valid;
-
-Scenarios.Authentication_ApiKey_invalid = validAndInvalidScenarios.invalid;
+Scenarios.Authentication_ApiKey_invalid = passOnSuccess({
+  uri: `/authentication/api-key/invalid`,
+  method: `get`,
+  request: {
+    headers: {
+      "x-ms-api-key": "valid-key",
+    },
+    status: 403,
+  },
+  response: {
+    status: 403,
+    body: json({
+      error: "invalid-api-key",
+    }),
+  },
+  handler: (req: MockRequest) => {
+    return {
+      status: 403,
+      body: json({
+        error: "invalid-api-key",
+      }),
+    };
+  },
+  kind: "MockApiDefinition",
+});

packages/cadl-ranch-specs/http/authentication/http/custom/mockapi.ts
@@ -1,17 +1,48 @@
-import { MockRequest } from "@azure-tools/cadl-ranch-api";
+import { json, MockRequest, passOnSuccess } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
-import { getValidAndInvalidScenarios } from "../../commonapi.js";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-const validAndInvalidScenarios = getValidAndInvalidScenarios(
-  "http/custom",
-  "invalid-api-key",
-  function addOptionalParamOldApiVersionNewClientValidate(req: MockRequest): void {
-    req.expect.containsHeader("authorization", "SharedAccessKey valid-key");
+Scenarios.Authentication_Http_Custom_valid = passOnSuccess({
+  uri: `/authentication/http/custom/valid`,
+  method: "get",
+  request: {
+    headers: {
+      authorization: "SharedAccessKey valid-key",
+    },
   },
-);
+  response: {
+    status: 204,
+  },
+  handler: (req: MockRequest) => {
+    req.expect.containsHeader("authorization", "SharedAccessKey valid-key");
+    return { status: 204 };
+  },
+  kind: "MockApiDefinition",
+});
 
-Scenarios.Authentication_Http_Custom_valid = validAndInvalidScenarios.valid;
-
-Scenarios.Authentication_Http_Custom_invalid = validAndInvalidScenarios.invalid;
+Scenarios.Authentication_Http_Custom_invalid = passOnSuccess({
+  uri: `/authentication/http/custom/invalid`,
+  method: "get",
+  request: {
+    headers: {
+      authorization: "SharedAccessKey valid-key",
+    },
+    status: 403,
+  },
+  response: {
+    status: 403,
+    body: json({
+      error: "invalid-api-key",
+    }),
+  },
+  handler: (req: MockRequest) => {
+    return {
+      status: 403,
+      body: json({
+        error: "invalid-api-key",
+      }),
+    };
+  },
+  kind: "MockApiDefinition",
+});

packages/cadl-ranch-specs/http/authentication/oauth2/mockapi.ts
@@ -1,17 +1,45 @@
-import { MockRequest } from "@azure-tools/cadl-ranch-api";
+import { json, MockRequest, passOnSuccess } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
-import { getValidAndInvalidScenarios } from "../commonapi.js";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-const validAndInvalidScenarios = getValidAndInvalidScenarios(
-  "oauth2",
-  "invalid-grant",
-  function addOptionalParamOldApiVersionNewClientValidate(req: MockRequest): void {
-    req.expect.containsHeader("authorization", "Bearer https://security.microsoft.com/.default");
+Scenarios.Authentication_OAuth2_valid = passOnSuccess({
+  uri: `/authentication/oauth2/valid`,
+  method: "get",
+  request: {
+    headers: {
+      authorization: "Bearer https://security.microsoft.com/.default",
+    },
   },
-);
+  response: {
+    status: 204,
+  },
+  handler: (req: MockRequest) => {
+    req.expect.containsHeader("authorization", "Bearer https://security.microsoft.com/.default");
+    return { status: 204 };
+  },
+  kind: "MockApiDefinition",
+});
 
-Scenarios.Authentication_OAuth2_valid = validAndInvalidScenarios.valid;
-
-Scenarios.Authentication_OAuth2_invalid = validAndInvalidScenarios.invalid;
+Scenarios.Authentication_OAuth2_invalid = passOnSuccess({
+  uri: `/authentication/oauth2/invalid`,
+  method: "get",
+  request: {
+    status: 403,
+  },
+  response: {
+    status: 403,
+    body: json({
+      error: "invalid-grant",
+    }),
+  },
+  handler: (req: MockRequest) => {
+    return {
+      status: 403,
+      body: json({
+        error: "invalid-grant",
+      }),
+    };
+  },
+  kind: "MockApiDefinition",
+});

commit cd2a4fc2574c160652ef379789d3696dc6aeb3a1

fix wrong mock api introduced by #741

packages/cadl-ranch-specs/http/azure/core/basic/mockapi.ts
@@ -111,10 +111,10 @@ Scenarios.Azure_Core_Basic_list = passOnSuccess({
     req.expect.containsQueryParam("skip", "10");
     req.expect.containsQueryParam("orderby", "id");
     req.expect.containsQueryParam("filter", "id lt 10");
-    if (!req.originalRequest.originalUrl.includes("select[]=id&select[]=orders&select[]=etag")) {
+    if (!req.originalRequest.originalUrl.includes("select=id&select=orders&select=etag")) {
       throw new ValidationError(
-        "Expected query param select[]=id&select[]=orders&select[]=etag ",
-        "select[]=id&select[]=orders&select[]=etag",
+        "Expected query param select=id&select=orders&select=etag ",
+        "select=id&select=orders&select=etag",
         req.originalRequest.originalUrl,
       );
     }

packages/cadl-ranch-specs/http/encode/bytes/mockapi.ts
@@ -217,11 +217,11 @@ Scenarios.Encode_Bytes_RequestBody_base64 = createRequestBodyServerTests(
 );
 Scenarios.Encode_Bytes_RequestBody_base64url = createRequestBodyServerTests(
   "/encode/bytes/body/request/base64url",
-  '"dGVzdA=="',
+  '"dGVzdA"',
   {
     "Content-Type": "application/json",
   },
-  '"dGVzdA=="',
+  '"dGVzdA"',
 );
 function createResponseBodyServerTests(
   uri: string,

packages/cadl-ranch-specs/http/parameters/body-optionality/mockapi.ts
@@ -48,16 +48,12 @@ Scenarios.Parameters_BodyOptionality_OptionalExplicit = passOnSuccess([
   {
     uri: "/parameters/body-optionality/optional-explicit/omit",
     method: "post",
-    request: {
-      body: {
-        name: "foo",
-      },
-    },
+    request: {},
     response: {
       status: 204,
     },
     handler: (req: MockRequest) => {
-      req.expect.bodyEquals({ name: "foo" });
+      req.expect.rawBodyEquals(undefined);
       return { status: 204 };
     },
     kind: "MockApiDefinition",