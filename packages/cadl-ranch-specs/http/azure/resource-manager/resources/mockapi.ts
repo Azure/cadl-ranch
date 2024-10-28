ba34294a660bc1695ab4c03e072db07f13720015
hongli750210 <v-hongli1@microsoft.com>
2024-10-28 16:13:28 +0800

    Reopen Test ARM, test case for location-based resource

--- packages/cadl-ranch-specs/http/azure/resource-manager/resources/mockapi.ts
+++ packages/cadl-ranch-specs/http/azure/resource-manager/resources/mockapi.ts
@@ -5,6 +5,7 @@ export const Scenarios: Record<string, ScenarioMockApi> = {};
 
 const SUBSCRIPTION_ID_EXPECTED = "00000000-0000-0000-0000-000000000000";
 const RESOURCE_GROUP_EXPECTED = "test-rg";
+const LOCATION_EXPECTED = "eastus";
 const validTopLevelResource = {
   id: `/subscriptions/${SUBSCRIPTION_ID_EXPECTED}/resourceGroups/${RESOURCE_GROUP_EXPECTED}/providers/Azure.ResourceManager.Resources/topLevelTrackedResources/top`,
   name: "top",
@@ -60,6 +61,134 @@ const validSingletonResource = {
     lastModifiedByType: "User",
   },
 };
+
+const validLocationResource = {
+  id: `/subscriptions/${SUBSCRIPTION_ID_EXPECTED}/locations/${LOCATION_EXPECTED}/providers/Azure.ResourceManager.Resources/locationResources/resource`,
+  name: "resource",
+  type: "Azure.ResourceManager.Resources/locationResources",
+  properties: {
+    description: "valid",
+    provisioningState: "Succeeded",
+  },
+  systemData: {
+    createdBy: "AzureSDK",
+    createdByType: "User",
+    createdAt: new Date(),
+    lastModifiedBy: "AzureSDK",
+    lastModifiedAt: new Date(),
+    lastModifiedByType: "User",
+  },
+};
+
+// location tracked resource
+Scenarios.Azure_ResourceManager_Resources_LocationResources_get = passOnSuccess({
+  uri: "/subscriptions/:subscriptionId/locations/:location/providers/Azure.ResourceManager.Resources/locationResources/:locationResourceName",
+  method: "get",
+  request: {
+    params: {
+      "subscriptionId": SUBSCRIPTION_ID_EXPECTED,
+      "location": LOCATION_EXPECTED,
+      "locationResourceName": "resource",
+      "api-version": "2023-12-01-preview",
+    },
+  },
+  response: {
+    status: 200,
+    body: json(validLocationResource),
+  },
+  kind: "MockApiDefinition",
+});
+
+Scenarios.Azure_ResourceManager_Resources_LocationResources_createOrUpdate = passOnSuccess({
+  uri: "/subscriptions/:subscriptionId/locations/:location/providers/Azure.ResourceManager.Resources/locationResources/:locationResourceName",
+  method: "put",
+  request: {
+    params: {
+      "subscriptionId": SUBSCRIPTION_ID_EXPECTED,
+      "location": LOCATION_EXPECTED,
+      "locationResourceName": "resource",
+      "api-version": "2023-12-01-preview",
+    },
+    body: {
+      properties: {
+        description: "valid",
+      },
+    },
+  },
+  response: {
+    status: 200,
+    body: json(validLocationResource),
+  },
+  kind: "MockApiDefinition",
+});
+
+Scenarios.Azure_ResourceManager_Resources_LocationResources_update = passOnSuccess({
+  uri: "/subscriptions/:subscriptionId/locations/:location/providers/Azure.ResourceManager.Resources/locationResources/:locationResourceName",
+  method: "patch",
+  request: {
+    params: {
+      "subscriptionId": SUBSCRIPTION_ID_EXPECTED,
+      "location": LOCATION_EXPECTED,
+      "locationResourceName": "resource",
+      "api-version": "2023-12-01-preview",
+    },
+    body: {
+      properties: {
+        description: "valid2",
+      },
+    },
+    headers: {
+      "Content-Type": "application/merge-patch+json",
+    },
+  },
+  response: {
+    status: 200,
+    body: json({
+      ...validLocationResource,
+      properties: {
+        provisioningState: "Succeeded",
+        description: "valid2",
+      },
+    }),
+  },
+  kind: "MockApiDefinition",
+});
+
+Scenarios.Azure_ResourceManager_Resources_LocationResources_delete = passOnSuccess({
+  uri: "/subscriptions/:subscriptionId/locations/:location/providers/Azure.ResourceManager.Resources/locationResources/:locationResourceName",
+  method: "delete",
+  request: {
+    params: {
+      "subscriptionId": SUBSCRIPTION_ID_EXPECTED,
+      "location": LOCATION_EXPECTED,
+      "locationResourceName": "resource",
+      "api-version": "2023-12-01-preview",
+    },
+  },
+  response: {
+    status: 204,
+  },
+  kind: "MockApiDefinition",
+});
+
+Scenarios.Azure_ResourceManager_Resources_LocationResources_listByParent = passOnSuccess({
+  uri: "/subscriptions/:subscriptionId/locations/:location/providers/Azure.ResourceManager.Resources/locationResources",
+  method: "get",
+  request: {
+    params: {
+      "subscriptionId": SUBSCRIPTION_ID_EXPECTED,
+      "location": LOCATION_EXPECTED,
+      "api-version": "2023-12-01-preview",
+    },
+  },
+  response: {
+    status: 200,
+    body: json({
+      value: [validLocationResource],
+    }),
+  },
+  kind: "MockApiDefinition",
+});
 
 // singleton tracked resource
 Scenarios.Azure_ResourceManager_Resources_Singleton_getByResourceGroup = passOnSuccess({
