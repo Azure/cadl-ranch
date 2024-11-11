7fb502f3f03eaa5c0a80bbcf7dd29022205ca303
Weidong Xu <weidxu@microsoft.com>
2024-11-11 15:30:49 +0800

    update to use MockApiDefinition

--- packages/cadl-ranch-specs/http/client/initialization/basic/mockapi.ts
+++ packages/cadl-ranch-specs/http/client/initialization/basic/mockapi.ts
@@ -3,10 +3,12 @@ import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Client_Initialization_Basic = passOnSuccess(
-  mockapi.post("/client/initialization/basic/sub-client/subclient1:action", (req) => {
-    return {
-      status: 204,
-    };
-  }),
-);
+Scenarios.Client_Initialization_Basic = passOnSuccess({
+  uri: "/client/initialization/basic/sub-client/subclient1:action",
+  method: "post",
+  request: {},
+  response: {
+    status: 204,
+  },
+  kind: "MockApiDefinition",
+});
