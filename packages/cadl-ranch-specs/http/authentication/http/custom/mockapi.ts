d4edbea7566b417991ad78571d1491db231f5320
Mingzhe Huang (from Dev Box) <mingzhehuang@microsoft.com>
2024-11-11 14:57:19 +0800

    fix(mockapi): dotnet compatibility failure
    dotnet uses different but compatible datetime format, so the comparison should use `coercedBodyEquals`

--- packages/cadl-ranch-specs/http/authentication/http/custom/mockapi.ts
+++ packages/cadl-ranch-specs/http/authentication/http/custom/mockapi.ts
@@ -1,4 +1,4 @@
-import { json, passOnSuccess, passOnCode } from "@azure-tools/cadl-ranch-api";
+import { json, passOnSuccess, passOnCode, MockRequest } from "@azure-tools/cadl-ranch-api";
 import { ScenarioMockApi } from "@azure-tools/cadl-ranch-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
@@ -31,6 +31,14 @@ Scenarios.Authentication_Http_Custom_invalid = passOnCode(403, {
     body: json({
       error: "invalid-api-key",
     }),
+  },
+  handler: (req: MockRequest) => {
+    return {
+      status: 403,
+      body: json({
+        error: "invalid-api-key",
+      }),
+    };
   },
   kind: "MockApiDefinition",
 });
